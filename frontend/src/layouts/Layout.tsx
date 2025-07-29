4dd8382e4bd1996f0001c69348522f3eb695538e
bamb14 <althcjstk08@gmail.com>
2025-07-29 12:43:51 +0900

    feat: NavBar 레이아웃 페이지에 적용

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,6 +1,9 @@
+import NavBar from "./navBar/NavBar";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
+            <NavBar />
             <main>{children}</main>
         </div>
     );
