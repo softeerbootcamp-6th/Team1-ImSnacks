81c3445c284873edcafac299bf27e6e76c557271
bamb14 <althcjstk08@gmail.com>
2025-07-29 12:33:27 +0900

    feat: Layout 컴포넌트 제작

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <main>{children}</main>
+        </div>
+    );
+};
+
+export default Layout;
