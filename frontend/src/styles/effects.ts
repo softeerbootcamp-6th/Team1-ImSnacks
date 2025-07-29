f48d89869a0441b5f96a86e3f20f198a259b8ebf
eunjin11 <whdmswls9207@gmail.com>
2025-07-29 17:36:45 +0900

    feat: effects 디자인 시스템 구현

---
+++ frontend/src/styles/effects.ts
@@ -0,0 +1,24 @@
+export const DropShadow = {
+  Ds100: {
+    dropShadow: '0px 4px 28px 0px #71778F1F',
+  },
+  Ds200: {
+    dropShadow: '0 4px 32px rgba(113, 119, 143, 0.20)',
+  },
+  DsGraph: {
+    dropShadow: '0 4px 28px rgba(56, 61, 72, 0.40)',
+  },
+};
+
+export const BackgroundBlur = {
+  BgBlur100: {
+    backdropFilter: 'blur(16px)',
+  },
+};
+
+export const Multiple = {
+  Tooltip_Neighbor: {
+    boxShadow: '0 2px 4px 0 rgba(65, 83, 115, 0.25) inset',
+    filter: 'drop-shadow(0 2px 4px rgba(172, 203, 240, 0.20))',
+  },
+};
