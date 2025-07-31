1aeb0cd50bbf42b200e2ff9c1fa0c94d2a42a219
eunjin11 <whdmswls9207@gmail.com>
2025-07-31 19:02:14 +0900

    feat: crops 구조 변경

--- frontend/src/constants/crops.ts
+++ frontend/src/constants/crops.ts
@@ -2,13 +2,31 @@ export const CROPS = [
   {
     id: 1,
     name: '포도',
+    lifeCycle: '휴면기',
+    date: 223,
   },
   {
     id: 2,
     name: '사과',
+    lifeCycle: '과실비대2기',
+    date: 13,
   },
   {
     id: 3,
-    name: '바나나',
+    name: '복숭아',
+    lifeCycle: '꽃눈분화기',
+    date: 93,
+  },
+  {
+    id: 4,
+    name: '감귤',
+    lifeCycle: '발아기',
+    date: 156,
+  },
+  {
+    id: 5,
+    name: '단감',
+    lifeCycle: '성숙 및 착색기',
+    date: 134,
   },
 ];
