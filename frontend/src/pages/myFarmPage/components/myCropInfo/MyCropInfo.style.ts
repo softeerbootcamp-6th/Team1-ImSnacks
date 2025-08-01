bfc02e74ea9e8d0df0b235a3f3c2c5faaf670fa5
eunjin11 <whdmswls9207@gmail.com>
2025-08-01 17:15:00 +0900

    feat: MyCropInfoHeader 기존 스타일 코드 삭제

--- frontend/src/pages/myFarmPage/components/myCropInfo/MyCropInfo.style.ts
+++ frontend/src/pages/myFarmPage/components/myCropInfo/MyCropInfo.style.ts
@@ -1,23 +1,9 @@
 import { Spacing } from '@/styles/spacing';
-import { Typography } from '@/styles/typography';
-import { GrayScale } from '@/styles/colors';
 import { css } from '@emotion/react';
 
 const MyCropInfoContainer = css`
   display: flex;
   flex-direction: column;
-`;
-
-const MyCropInfoHeader = css`
-  height: 52px;
-  gap: ${Spacing.Spacing200};
-  padding: ${Spacing.Spacing300};
-  ${Typography.Subtitle_500};
-  color: ${GrayScale.White};
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  ${Typography.Subtitle_500};
 `;
 
 const CropInfoCardContainer = css`
@@ -29,6 +15,5 @@ const CropInfoCardContainer = css`
 
 export default {
   MyCropInfoContainer,
-  MyCropInfoHeader,
   CropInfoCardContainer,
 };
