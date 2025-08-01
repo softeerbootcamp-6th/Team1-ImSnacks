b6480418ba5afe3f89e5bc8f257a447c8645afb8
bamb14 <althcjstk08@gmail.com>
2025-08-01 18:50:43 +0900

    feat: MainPageGraph 컴포넌트 구현

---
+++ frontend/src/pages/homePage/components/mainPageGraph/MainPageGraph.styles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+
+const MainPageGraph = css`
+  flex-shrink: 0;
+`;
+
+export default {
+  MainPageGraph,
+};
---
+++ frontend/src/pages/homePage/components/mainPageGraph/MainPageGraph.tsx
@@ -0,0 +1,11 @@
+import GraphMenu from '../graphMenu/GraphMenu';
+
+const MainPageGraph = () => {
+  return (
+    <div>
+      <GraphMenu />
+      Main Page Graph
+    </div>
+  );
+};
+export default MainPageGraph;
