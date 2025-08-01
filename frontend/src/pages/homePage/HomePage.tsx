f6fbb7892b8e68a2165af6c3791a0effdfcecc79
bamb14 <althcjstk08@gmail.com>
2025-08-01 18:52:05 +0900

    refactor: 홈페이지 백그라운 스타일링 제거 및 컴포넌트 추가

--- frontend/src/pages/homePage/HomePage.tsx
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -1,21 +1,22 @@
 import { css } from '@emotion/react';
-import { gradientStyles } from '@/styles/gradientStyles';
 import RegisterWorkContainer from './components/registerWorkContainer/RegisterWorkContainer';
 import WorkContainer from './components/workContainer/WorkContainer';
+import HeadLine from './components/headLine/HeadLine';
+import MainPageGraph from './components/mainPageGraph/MainPageGraph';
 
 const HomePage = () => {
   return (
     <div
       css={css`
-        ${gradientStyles.backgroundAfternoonClear}
-        width: 100vw;
-        height: 100vh;
+        margin-top: 172px;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
       `}
     >
+      <HeadLine />
+      <MainPageGraph />
       <WorkContainer />
       <RegisterWorkContainer />
     </div>
