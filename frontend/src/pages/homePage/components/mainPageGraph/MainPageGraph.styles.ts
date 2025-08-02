1bd7f911a9b1a98d07619f6af1d7b720e9588226
bamb14 <118671229+bamb14@users.noreply.github.com>
2025-08-02 16:09:41 +0900

    feat: MainPageGraph 스타일링 적용

--- frontend/src/pages/homePage/components/mainPageGraph/MainPageGraph.styles.ts
+++ frontend/src/pages/homePage/components/mainPageGraph/MainPageGraph.styles.ts
@@ -1,7 +1,17 @@
 import { css } from '@emotion/react';
 
 const MainPageGraph = css`
+  margin-top: 32px;
+  width: 1356px;
+  height: 338px;
   flex-shrink: 0;
+  position: relative;
+  border: 1px solid red;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default {
--- frontend/src/pages/homePage/components/mainPageGraph/MainPageGraph.tsx
+++ frontend/src/pages/homePage/components/mainPageGraph/MainPageGraph.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import GraphMenu from '../graphMenu/GraphMenu';
+import S from './MainPageGraph.styles';
 
 const MainPageGraph = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
   return (
-    <div>
-      <GraphMenu />
+    <div css={S.MainPageGraph}>
+      <GraphMenu tabIndex={tabIndex} setTabIndex={setTabIndex} />
       Main Page Graph
     </div>
   );
