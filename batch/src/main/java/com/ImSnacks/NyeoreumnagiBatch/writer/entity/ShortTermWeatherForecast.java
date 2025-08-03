c3b10713d817431dace6f0a23d90e1fd7baebdaa
adorableco <tpdusdl1001@knu.ac.kr>
2025-08-03 11:49:30 +0900

    fix: import 문 asterisk 삭제

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/writer/entity/ShortTermWeatherForecast.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/writer/entity/ShortTermWeatherForecast.java
@@ -1,7 +1,16 @@
 package com.ImSnacks.NyeoreumnagiBatch.writer.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
 @Entity
 @Table(name = "ShortTermWeatherForecast")
 @IdClass(ShortTermWeatherForecastId.class)
