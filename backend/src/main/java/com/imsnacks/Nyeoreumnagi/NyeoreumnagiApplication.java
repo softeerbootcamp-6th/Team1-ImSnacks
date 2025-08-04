1a69279f59d3f3b546ad61389edfbd60c81246e1
lim <gyuhyun.lim@outlook.com>
2025-08-04 23:42:13 +0900

    chore: git cache를 지운 후 파일들을 git이 다시 추적하도록 함

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/NyeoreumnagiApplication.java
@@ -0,0 +1,13 @@
+package com.imsnacks.Nyeoreumnagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyeoreumnagiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyeoreumnagiApplication.class, args);
+	}
+
+}
