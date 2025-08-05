fe60f28c6b7ed57a19034f67c8c7884669f0b570
eunjin11 <whdmswls9207@gmail.com>
2025-08-05 21:28:50 +0900

    feat: WorkSchedule dummydata

---
+++ frontend/src/constants/workScheduleData.ts
@@ -0,0 +1,18 @@
+export const WORK_SCHEDULE_DATA = [
+  {
+    id: 1,
+    cropName: '포도',
+    workName: '맛있게 먹기',
+    date: '2025-08-08',
+    workTime: '10:00 - 12:00',
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    cropName: '배',
+    workName: '배 맛있다',
+    date: '2025-08-09',
+    workTime: '13:00 - 15:00',
+    isCompleted: true,
+  },
+];
