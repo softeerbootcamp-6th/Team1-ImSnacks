54ff87e47bfb154b938a4d33fb822c2789e4528e
imscow11253 <seaweed8759@naver.com>
2025-08-05 16:17:43 +0900

    feat: 서리/이상고온 기상 특보 판단 필터 구현체 로직 작성 (리팩토링 전)

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/utils/weatherRiskFilter/TemperatureFilter.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/utils/weatherRiskFilter/TemperatureFilter.java
@@ -1,6 +1,7 @@
 package com.ImSnacks.NyeoreumnagiBatch.utils.weatherRiskFilter;
 
 import com.ImSnacks.NyeoreumnagiBatch.dto.VilageFcstResponse;
+import com.ImSnacks.NyeoreumnagiBatch.utils.ForecastTimeUtils;
 import com.ImSnacks.NyeoreumnagiBatch.writer.dto.ShortTermWeatherDto;
 import com.ImSnacks.NyeoreumnagiBatch.writer.entity.WeatherRiskType;
 import org.springframework.stereotype.Component;
@@ -23,7 +24,34 @@ public class TemperatureFilter extends WeatherRiskFilter{
     public List<ShortTermWeatherDto.WeatherRiskDto> filtering(Map<String, List<VilageFcstResponse.Item>> metrics) {
         Map<Integer, WeatherRiskType> riskPerTime = new HashMap<>();
 
+        List<Map.Entry<String, List<VilageFcstResponse.Item>>> entryList = new ArrayList<>(metrics.entrySet());
+        for(int i =0;i<entryList.size();i++){
+            Map.Entry<String, List<VilageFcstResponse.Item>> entry1 = entryList.get(i);
+            VilageFcstResponse.Item item1 = entry1.getValue().stream()
+                    .filter(it -> it.getCategory().equals(metricCategory))
+                    .findFirst()
+                    .orElse(null);
 
+            Map.Entry<String, List<VilageFcstResponse.Item>> entry2 = entryList.get(i+1);
+            VilageFcstResponse.Item item2 = entry2.getValue().stream()
+                    .filter(it -> it.getCategory().equals(metricCategory))
+                    .findFirst()
+                    .orElse(null);
+
+            if(item1 == null || item2 == null){
+                continue;
+            }
+
+            for(Map.Entry<WeatherRiskType, BiPredicate<String, String>> entry : weatherRiskMapper.entrySet()){
+                if(entry.getValue().test(item1.getFcstValue(), item2.getFcstValue())){
+                    if(entry.getKey().equals(WeatherRiskType.FROST)){
+                        riskPerTime.put(ForecastTimeUtils.getIntegerFromAPITime(entry1.getKey()), entry.getKey());
+                        riskPerTime.put(ForecastTimeUtils.getIntegerFromAPITime(entry2.getKey()), entry.getKey());
+                    }
+                    riskPerTime.put(i, entry.getKey());
+                }
+            }
+        }
 
         return groupingSameContinuousRisk(riskPerTime);
     }
