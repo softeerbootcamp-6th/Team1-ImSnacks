537e31df9d512d83f87fe100e506135892ebcacd
Seyeon Park <tpdusdl1001@gmail.com>
2025-08-05 16:11:42 +0900

    feat: MyWork(내 농작업) Entity 구성

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/my_work/WorkStatus.java
@@ -0,0 +1,6 @@
+package com.imsnacks.Nyeoreumnagi.my_work;
+
+public enum WorkStatus {
+    COMPLETED,
+    NOT_COMPLETED,
+}
---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/my_work/entity/MyWork.java
@@ -0,0 +1,35 @@
+package com.imsnacks.Nyeoreumnagi.my_work.entity;
+
+import com.imsnacks.Nyeoreumnagi.my_work.WorkStatus;
+import com.imsnacks.Nyeoreumnagi.recommended_work.entity.RecommendedWork;
+import jakarta.persistence.*;
+
+@Entity
+public class MyWork {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+//    @ManyToOne
+//    private Member member;
+
+    @OneToOne
+    private RecommendedWork recommendedWork;
+
+    @Column(name = "start_time", nullable = false)
+    int startTime;
+
+    @Column(name = "end_time", nullable = false)
+    int endTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    WorkStatus workStatus;
+
+    @Column(name = "crop_name", nullable = false)
+    String cropName;
+
+}
