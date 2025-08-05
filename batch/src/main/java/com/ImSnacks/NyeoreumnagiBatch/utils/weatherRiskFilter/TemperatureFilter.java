edfc662363c7b70a597a9b8965ec2c0d3066d446
imscow11253 <seaweed8759@naver.com>
2025-08-05 18:08:57 +0900

    hotfix: 기온 필터 클래스에서 마지막 요소에 대해 이상고온 판단이 안되는 문제 해결

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/utils/weatherRiskFilter/TemperatureFilter.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/utils/weatherRiskFilter/TemperatureFilter.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.function.BiPredicate;
 
 @Component
-public class TemperatureFilter extends WeatherRiskFilter{
+public class TemperatureFilter extends WeatherRiskFilter {
     private static final String metricCategory = "TMP";
     private static final Map<WeatherRiskType, BiPredicate<String, String>> weatherRiskMapper = Map.of(
             WeatherRiskType.FROST, (value1, value2) -> Integer.parseInt(value1) <= -2 && Integer.parseInt(value2) <= -2,
@@ -25,30 +25,46 @@ public class TemperatureFilter extends WeatherRiskFilter{
         Map<Integer, WeatherRiskType> riskPerTime = new HashMap<>();
 
         List<Map.Entry<String, List<VilageFcstResponse.Item>>> entryList = new ArrayList<>(metrics.entrySet());
-        for(int i =0;i<entryList.size();i++){
+        for (int i = 0; i < entryList.size(); i++) {
             Map.Entry<String, List<VilageFcstResponse.Item>> entry1 = entryList.get(i);
             VilageFcstResponse.Item item1 = entry1.getValue().stream()
                     .filter(it -> it.getCategory().equals(metricCategory))
                     .findFirst()
                     .orElse(null);
 
-            Map.Entry<String, List<VilageFcstResponse.Item>> entry2 = entryList.get(i+1);
+            if (item1 == null) {
+                continue;
+            }
+
+            //마지막 요소는 이상고온 판단만 한다.
+            if (i == entryList.size() - 1) {
+                BiPredicate<String, String> abnormalHeatPredicate = weatherRiskMapper.get(WeatherRiskType.ABNORMAL_HEAT);
+                if (abnormalHeatPredicate.test(item1.getFcstValue(), null)) {
+                    int hour = ForecastTimeUtils.getIntegerFromAPITime(entry1.getKey());
+                    riskPerTime.put(hour, WeatherRiskType.ABNORMAL_HEAT);
+                }
+                continue;
+            }
+
+            Map.Entry<String, List<VilageFcstResponse.Item>> entry2 = entryList.get(i + 1);
             VilageFcstResponse.Item item2 = entry2.getValue().stream()
                     .filter(it -> it.getCategory().equals(metricCategory))
                     .findFirst()
                     .orElse(null);
 
-            if(item1 == null || item2 == null){
+            if (item2 == null) {
                 continue;
             }
 
-            for(Map.Entry<WeatherRiskType, BiPredicate<String, String>> entry : weatherRiskMapper.entrySet()){
-                if(entry.getValue().test(item1.getFcstValue(), item2.getFcstValue())){
-                    if(entry.getKey().equals(WeatherRiskType.FROST)){
+            for (Map.Entry<WeatherRiskType, BiPredicate<String, String>> entry : weatherRiskMapper.entrySet()) {
+                if (entry.getValue().test(item1.getFcstValue(), item2.getFcstValue())) {
+                    if (entry.getKey().equals(WeatherRiskType.FROST)) {
                         riskPerTime.put(ForecastTimeUtils.getIntegerFromAPITime(entry1.getKey()), entry.getKey());
                         riskPerTime.put(ForecastTimeUtils.getIntegerFromAPITime(entry2.getKey()), entry.getKey());
+                    } else {
+                        riskPerTime.put(ForecastTimeUtils.getIntegerFromAPITime(entry1.getKey()), entry.getKey());
                     }
-                    riskPerTime.put(i, entry.getKey());
+                    break;
                 }
             }
         }
