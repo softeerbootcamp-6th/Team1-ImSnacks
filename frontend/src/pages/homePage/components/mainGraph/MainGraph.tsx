18ce2865ea7c601bd835977b06d1beb679350cdf
bamb14 <althcjstk08@gmail.com>
2025-08-05 14:09:59 +0900

    test: unused lint 테스트

--- frontend/src/pages/homePage/components/mainGraph/MainGraph.tsx
+++ frontend/src/pages/homePage/components/mainGraph/MainGraph.tsx
@@ -1,6 +1,16 @@
 import { useState } from 'react';
 import GraphMenu from '../graphMenu/GraphMenu';
 import S from './MainGraph.style';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 const MainGraph = () => {
   const [tabIndex, setTabIndex] = useState(0);
@@ -8,7 +18,6 @@ const MainGraph = () => {
   return (
     <div css={S.MainGraph}>
       <GraphMenu tabIndex={tabIndex} setTabIndex={setTabIndex} />
-      Main Page Graph
     </div>
   );
 };
