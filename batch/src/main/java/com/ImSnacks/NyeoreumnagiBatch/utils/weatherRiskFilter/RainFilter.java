8159f3bfa07bb3dc0e01352cfd37368c3ff332ba
imscow11253 <seaweed8759@naver.com>
2025-08-05 11:48:40 +0900

    feat: 기상 특보 필터 구현체 생성

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/utils/weatherRiskFilter/RainFilter.java
@@ -0,0 +1,18 @@
+package com.ImSnacks.NyeoreumnagiBatch.utils.weatherRiskFilter;
+
+import com.ImSnacks.NyeoreumnagiBatch.writer.dto.ShortTermWeatherDto;
+import dto.VilageFcstResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RainFilter implements WeatherRiskFilter{
+    @Override
+    public List<ShortTermWeatherDto.WeatherRisk> filter(List<VilageFcstResponse.Item> metrics) {
+        List<ShortTermWeatherDto.WeatherRisk> risks = new ArrayList<>();
+
+
+
+        return risks;
+    }
+}
