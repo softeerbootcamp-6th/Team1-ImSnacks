731c081efbe85c2fa365af4f1bda235a2bec9a62
lim <gyuhyun.lim@outlook.com>
2025-08-05 16:11:29 +0900

    chore: logger 삭제

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/reader/WeatherReader.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/reader/WeatherReader.java
@@ -5,7 +5,6 @@ import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
@@ -20,13 +19,10 @@ import java.util.List;
 
 //TODO 아직 오류에 대한 예외처리는 하지 않음. -> Job은 예외를 외부로 던지면 안 됨. Job은 수행 중 발생한 오류를 JobRepository에 보고할 의무를 진다.
 //TODO Batch Job 예외 처리 찾아보기
-@Slf4j
 @Component
 @StepScope // job parameters 주입을 위해 빈 생성 시점을 step 생성 시점으로 미룬다.
 public class WeatherReader implements ItemReader<VilageFcstResponseDto> {
 
-    // Job 호출 시, Step 구성 시에 Reader 클래스가 어떻게 호출되는지 몰라서, 일단 static 선언
-    // 추후에 위치 값 호출 방식 변경하기 -> DB 또는 CSV 파일?
     private static List<NxNy>  locations = null;
 
     // thread-safe 필요
@@ -38,11 +34,6 @@ public class WeatherReader implements ItemReader<VilageFcstResponseDto> {
     private final JdbcTemplate jdbcTemplate;
     @Autowired
     private ApiCaller apiCaller;
-    /*
-    실패시 null 반환
-    * */
-
-
 
     WeatherReader(@Value("#{jobParameters['baseDate']}") String baseDate,
                   @Value("#{jobParameters['baseTime']}") String baseTime,
@@ -70,9 +61,6 @@ public class WeatherReader implements ItemReader<VilageFcstResponseDto> {
 
         try {
             VilageFcstResponseDto dto = apiCaller.call(baseDate, baseTime, nx, ny);
-//            log.info(dto.toString());
-//            log.info("\n\n");
-
             return dto;
         } catch (Exception e) {
             return null;
