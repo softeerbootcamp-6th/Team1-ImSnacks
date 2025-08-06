86563c8481aa25c53c1f1b1afbd6121344426c1e
eunjin11 <whdmswls9207@gmail.com>
2025-08-06 16:06:37 +0900

    refactor: WorkCardWeb, WorkSchedule style 코드 분리

--- frontend/src/pages/myFarmPage/components/workCardWeb/WorkCardWeb.tsx
+++ frontend/src/pages/myFarmPage/components/workCardWeb/WorkCardWeb.tsx
@@ -1,11 +1,6 @@
-import { Assets, ColorStatus, GrayScale } from '@/styles/colors';
-import { BorderRadius } from '@/styles/borderRadius';
-import { Spacing } from '@/styles/spacing';
-import { css } from '@emotion/react';
-import { Typography } from '@/styles/typography';
 import WorkChip from '../workChip/WorkChip';
 import { WORK_CHIP_TYPES } from '@/types/workChip.type';
-import { CROP_NAME } from '@/constants/cropName';
+import S from './WorkCardWeb.style';
 
 interface WorkCardWebProps {
   cropName: string;
@@ -21,67 +16,13 @@ const WorkCardWeb = ({
   isCompleted,
 }: WorkCardWebProps) => {
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        gap: ${Spacing.Spacing300};
-        padding: ${Spacing.Spacing200} ${Spacing.Spacing300};
-        border-radius: ${BorderRadius.Base.S_Hard};
-        background-color: ${GrayScale.White};
-        border: 1px solid ${GrayScale.G200};
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: row;
-          gap: ${Spacing.Spacing300};
-        `}
-      >
-        <div
-          css={css`
-            width: 4px;
-            height: 76px;
-            background-color: ${ColorStatus.Crops[
-              CROP_NAME[
-                cropName as keyof typeof CROP_NAME
-              ] as keyof typeof ColorStatus.Crops
-            ]};
-            border-radius: ${BorderRadius.Base.Hard};
-          `}
-        />
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            gap: ${Spacing.Spacing100};
-          `}
-        >
-          <div
-            css={css`
-              ${Typography.Body_L_500}
-              color: ${Assets.Text.WorkCard.Default.Headline};
-            `}
-          >
-            {workName}
-          </div>
-          <div
-            css={css`
-              ${Typography.Body_S_400}
-              color: ${Assets.Text.WorkCard.Default.Headline};
-            `}
-          >
-            {cropName}
-          </div>
-          <div
-            css={css`
-              ${Typography.Caption_S}
-              color: ${Assets.Text.WorkCard.Default.Body};
-            `}
-          >
-            {workTime}
-          </div>
+    <div css={S.WorkCardContainer}>
+      <div css={S.WorkCardContent}>
+        <div css={S.WorkCardColorBar(cropName)} />
+        <div css={S.WorkCardInfo}>
+          <div css={S.WorkCardTitle}>{workName}</div>
+          <div css={S.WorkCardCropName}>{cropName}</div>
+          <div css={S.WorkCardTime}>{workTime}</div>
         </div>
       </div>
       <WorkChip
--- frontend/src/pages/myFarmPage/components/workSchedule/WorkSchedule.tsx
+++ frontend/src/pages/myFarmPage/components/workSchedule/WorkSchedule.tsx
@@ -1,15 +1,10 @@
-import { css } from '@emotion/react';
 import MyFarmHeader from '../myFarmHeader/MyFarmHeader';
 import WeeklyCalendar from '../weeklyCalendar/WeeklyCalendar';
 import WeekNavigator from '../weekNavigator/WeekNavigator';
 import { useWeeklyCalendar } from '../../../../hooks/useWeeklyCalendar';
-import { Spacing } from '@/styles/spacing';
-import { BorderRadius } from '@/styles/borderRadius';
-import { customBorderGradientStyles } from '@/styles/customBorderGradientStyles';
-import { GrayScale, Opacity } from '@/styles/colors';
-import { Typography } from '@/styles/typography';
 import { WORK_SCHEDULE_DATA } from '@/constants/workScheduleData';
 import { groupDataRecordStructure } from '@/utils/groupDataRecord';
+import S from './WorkSchedule.style';
 
 const WorkSchedule = () => {
   const {
@@ -29,35 +24,10 @@ const WorkSchedule = () => {
   );
 
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-      `}
-    >
+    <div css={S.WorkScheduleContainer}>
       <MyFarmHeader title="작업 이력" />
-      <div
-        css={css`
-          height: 770px;
-          display: flex;
-          flex-direction: column;
-          border-radius: ${BorderRadius.Base.S_Hard};
-          ${customBorderGradientStyles.gradientBorder}
-          padding: ${Spacing.Spacing300};
-          gap: ${Spacing.Spacing300};
-          background-color: ${Opacity.White.W100};
-        `}
-      >
-        <div
-          css={css`
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            ${Typography.Body_L_500}
-            color: ${GrayScale.White};
-            padding: 0 ${Spacing.Spacing400} 0 ${Spacing.Spacing400};
-          `}
-        >
+      <div css={S.WorkScheduleContent}>
+        <div css={S.WorkScheduleHeader}>
           <span>{monthLabel}</span>
           <WeekNavigator
             weekLabel={weekLabel}
