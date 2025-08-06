a06bbe1e5278a8bd4c469205592f96c8f3c1c701
Seyeon Park <tpdusdl1001@gmail.com>
2025-08-06 12:16:39 +0900

    feat: WeatherJobConfig 구현

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/WeatherJobConfig.java
@@ -0,0 +1,44 @@
+package com.ImSnacks.NyeoreumnagiBatch;
+
+import com.ImSnacks.NyeoreumnagiBatch.processor.WeatherProcessor;
+import com.ImSnacks.NyeoreumnagiBatch.reader.WeatherReader;
+import com.ImSnacks.NyeoreumnagiBatch.reader.dto.VilageFcstResponseDto;
+import com.ImSnacks.NyeoreumnagiBatch.writer.WeatherWriter;
+import com.ImSnacks.NyeoreumnagiBatch.writer.dto.ShortTermWeatherDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class WeatherJobConfig {
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final WeatherReader weatherReader;
+    private final WeatherWriter weatherWriter;
+    private final WeatherProcessor weatherProcessor;
+
+
+    @Bean
+    public Job weatherJob(JobRepository jobRepository, Step weatherStep) {
+        return new JobBuilder("weatherJob", jobRepository)
+                .start(weatherStep)
+                .build();
+    }
+
+    @Bean
+    public Step weatherStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("weatherStep", jobRepository)
+                .<VilageFcstResponseDto, ShortTermWeatherDto>chunk(100, transactionManager)
+                .reader(weatherReader)
+                .processor(weatherProcessor)
+                .writer(weatherWriter)
+                .build();
+    }
+}
