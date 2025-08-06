02956d1a0cce782c5b04eea8a6a685a6e009887c
bamb14 <althcjstk08@gmail.com>
2025-08-06 16:22:30 +0900

    design: MainGraph 스타일속성 제거

--- frontend/src/pages/homePage/components/mainGraph/MainGraph.style.ts
+++ frontend/src/pages/homePage/components/mainGraph/MainGraph.style.ts
@@ -1,12 +1,9 @@
 import { css } from '@emotion/react';
 
 const MainGraph = css`
-  margin-top: 32px;
   width: 1356px;
-  height: 338px;
   flex-shrink: 0;
   position: relative;
-  border: 1px solid red;
 
   display: flex;
   flex-direction: column;
