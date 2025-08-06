9b182b2279d0a6e0dcb13cf18ebdf5772e283a9d
eunjin11 <whdmswls9207@gmail.com>
2025-08-06 15:26:22 +0900

    feat: workChip Status 삭제, 스타일 수정

--- frontend/src/pages/myFarmPage/components/workChip/WorkChip.style.ts
+++ frontend/src/pages/myFarmPage/components/workChip/WorkChip.style.ts
@@ -9,20 +9,6 @@ import {
   type WorkChipStatus,
 } from '@/types/workCardWeb.type';
 
-const WorkChip = css`
-  ${FlexStyles.flexRow}
-  width: fit-content;
-  gap: ${Spacing.Spacing100};
-  padding: ${Spacing.Spacing100} ${Spacing.Spacing300};
-  border-radius: ${BorderRadius.Base.Hard};
-  margin-left: auto;
-`;
-
-const WorkChipDot = css`
-  width: 4px;
-  height: 4px;
-  border-radius: ${BorderRadius.Base.Hard};
-`;
 const getWorkChipColorByType = (
   type: WorkChipType,
   status: WorkChipStatus
@@ -41,6 +27,39 @@ const getWorkChipDotColorByType = (
     : incompleteWorkChipDotColorByStatus[status]}
 `;
 
+const WorkChip = (type: WorkChipType) => css`
+  ${FlexStyles.flexRow}
+  width: fit-content;
+  gap: ${Spacing.Spacing100};
+  padding: ${Spacing.Spacing100} ${Spacing.Spacing300};
+  border-radius: ${BorderRadius.Base.Hard};
+  margin-left: auto;
+  ${getWorkChipColorByType(type, 'Default')};
+
+  &:hover {
+    ${getWorkChipColorByType(type, 'Hover')}
+  }
+
+  &:active {
+    ${getWorkChipColorByType(type, 'Pressed')}
+  }
+`;
+
+const WorkChipDot = (type: WorkChipType) => css`
+  width: 4px;
+  height: 4px;
+  border-radius: ${BorderRadius.Base.Hard};
+  ${getWorkChipDotColorByType(type, 'Default')};
+
+  button:hover & {
+    ${getWorkChipDotColorByType(type, 'Hover')}
+  }
+
+  button:active & {
+    ${getWorkChipDotColorByType(type, 'Pressed')}
+  }
+`;
+
 const completeWorkChipDotColorByStatus = {
   Default: css`
     background-color: ${ColorStatus.Global.Green};
@@ -49,7 +68,7 @@ const completeWorkChipDotColorByStatus = {
     background-color: ${GrayScale.White};
   `,
   Hover: css`
-    background-color: ${GrayScale.White};
+    background-color: ${ColorStatus.Global.Green};
   `,
 };
 
@@ -86,18 +105,16 @@ const incompleteWorkChipColorByStatus = {
     color: ${GrayScale.G800};
   `,
   Pressed: css`
-    background-color: ${Opacity.G500['5o400']};
-    color: ${GrayScale.G800};
-  `,
-  Hover: css`
     background-color: ${GrayScale.G800};
     color: ${GrayScale.White};
+  `,
+  Hover: css`
+    background-color: ${Opacity.G500['5o400']};
+    color: ${GrayScale.G800};
   `,
 };
 
 export default {
   WorkChip,
   WorkChipDot,
-  getWorkChipColorByType,
-  getWorkChipDotColorByType,
 };
--- frontend/src/pages/myFarmPage/components/workChip/WorkChip.tsx
+++ frontend/src/pages/myFarmPage/components/workChip/WorkChip.tsx
@@ -1,25 +1,15 @@
-import {
-  WORK_CHIP_TYPES,
-  type WorkChipType,
-  type WorkChipStatus,
-} from '@/types/workCardWeb.type';
+import { WORK_CHIP_TYPES, type WorkChipType } from '@/types/workCardWeb.type';
 import S from './WorkChip.style';
 import type { ButtonHTMLAttributes } from 'react';
 
 export interface WorkChipProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   chipType: WorkChipType;
-  status: WorkChipStatus;
 }
 
-const WorkChip = ({ chipType, status, ...props }: WorkChipProps) => {
+const WorkChip = ({ chipType, ...props }: WorkChipProps) => {
   return (
-    <button
-      css={[S.WorkChip, S.getWorkChipColorByType(chipType, status)]}
-      {...props}
-    >
-      <div
-        css={[S.WorkChipDot, S.getWorkChipDotColorByType(chipType, status)]}
-      />
+    <button css={S.WorkChip(chipType)} {...props}>
+      <div css={S.WorkChipDot(chipType)} />
       {chipType === WORK_CHIP_TYPES.COMPLETE ? '완료' : '미완료'}
     </button>
   );
