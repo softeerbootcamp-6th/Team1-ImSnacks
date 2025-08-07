e61ea3b4b38d9e64aed80c4c551387128a42ae75
imscow11253 <seaweed8759@naver.com>
2025-08-07 18:43:32 +0900

    feat: WeatherController가 쿼리 파라미터를 enum으로 받게 구현

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/WeatherController.java
@@ -0,0 +1,26 @@
+package com.imsnacks.Nyeoreumnagi.weather;
+
+import com.imsnacks.Nyeoreumnagi.common.enums.WeatherMetric;
+import com.imsnacks.Nyeoreumnagi.common.response.CustomResponseBody;
+import com.imsnacks.Nyeoreumnagi.common.auth.annotation.PreAuthorize;
+import com.imsnacks.Nyeoreumnagi.common.response.ResponseUtil;
+import com.imsnacks.Nyeoreumnagi.weather.dto.response.GetWeatherGraphResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @GetMapping()
+    public ResponseEntity<CustomResponseBody<GetWeatherGraphResponse>> getWeatherGraph(@PreAuthorize Long memberId, @RequestParam WeatherMetric weatherMetric){
+        return ResponseUtil.success(weatherService.getWeatherGraph(memberId, weatherMetric));
+    }
+}
