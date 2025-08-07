394a32304750ea8f4f3a3aaad033ac856a5c8a1b
imscow11253 <seaweed8759@naver.com>
2025-08-07 20:16:07 +0900

    refactor: repository 클래스명 변경

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/ShortTermWeatherForecastRepository.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/ShortTermWeatherForecastRepository.java
@@ -4,5 +4,5 @@ import com.imsnacks.Nyeoreumnagi.weather.entity.ShortTermWeatherForecast;
 import com.imsnacks.Nyeoreumnagi.weather.entity.ShortTermWeatherForecastId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WeatherRepository extends JpaRepository<ShortTermWeatherForecast, ShortTermWeatherForecastId> {
+public interface ShortTermWeatherForecastRepository extends JpaRepository<ShortTermWeatherForecast, ShortTermWeatherForecastId> {
 }
