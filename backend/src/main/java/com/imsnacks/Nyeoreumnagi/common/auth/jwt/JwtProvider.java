f4ecad06f1380df25c459b4b0db13e83c878756c
imscow11253 <seaweed8759@naver.com>
2025-08-07 12:22:20 +0900

    feat: JWT 토큰 생성 및 파싱 로직을 담당하는 JwtProvider 클래스 구현

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/auth/jwt/JwtProvider.java
@@ -0,0 +1,59 @@
+package com.imsnacks.Nyeoreumnagi.common.auth.jwt;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JwtProvider {
+
+    @Value("${secret.jwt-secret-key}")
+    private String JWT_SECRET_KEY;
+
+    @Value("${secret.jwt-expired-in.access-token}")
+    private long JWT_EXPIRED_IN;
+
+    public AuthTokens createToken(long memberId) {
+        Claims claims = Jwts.claims()
+                .setSubject(String.valueOf(memberId))
+                .setIssuer("nyeoreumnagi");
+
+        Date now = new Date();
+        Date accessTokenExpiredAt = new Date(now.getTime() + JWT_EXPIRED_IN);
+
+        String accessToken = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(accessTokenExpiredAt)
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
+                .compact();
+
+        UUID refreshToken = UUID.randomUUID();
+
+        return AuthTokens.of(accessToken, refreshToken, JWT_EXPIRED_IN);
+    }
+
+    public boolean isExpiredToken(String token){
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(JWT_SECRET_KEY).build()
+                    .parseClaimsJws(token);
+            return claims.getBody().getExpiration().before(new Date());
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | IllegalArgumentException e) {
+            return true;
+        }
+    }
+
+    public Long getMemberId(String token) {
+        String rawMemberId=  Jwts.parserBuilder()
+                .setSigningKey(JWT_SECRET_KEY).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        return Long.parseLong(rawMemberId);
+    }
+}
