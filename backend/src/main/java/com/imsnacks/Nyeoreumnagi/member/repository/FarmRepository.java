56ba6f79e4ff09db381b663e294221b257b7c709
imscow11253 <seaweed8759@naver.com>
2025-08-07 20:50:33 +0900

    feat: 농장 엔티티를 위한 jpa repository 인터페이스 생성

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/repository/FarmRepository.java
@@ -0,0 +1,7 @@
+package com.imsnacks.Nyeoreumnagi.member.repository;
+
+import com.imsnacks.Nyeoreumnagi.member.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmRepository extends JpaRepository<Farm, Long> {
+}
