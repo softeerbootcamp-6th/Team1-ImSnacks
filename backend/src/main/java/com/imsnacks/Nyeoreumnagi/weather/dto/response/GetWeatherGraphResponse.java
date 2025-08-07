d4be4de4583a74b07ac7faf875e6c7108da0e6e9
imscow11253 <seaweed8759@naver.com>
2025-08-07 19:57:20 +0900

    feat: 기상 그래프를 위한 DTO 생성

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/dto/response/GetWeatherGraphResponse.java
@@ -0,0 +1,16 @@
+package com.imsnacks.Nyeoreumnagi.weather.dto.response;
+
+import java.util.List;
+
+public record GetWeatherGraphResponse (
+        int max,
+        int min,
+        String weatherMetric,
+        List<ValuePerTime> valuePerTime
+){
+    public static record ValuePerTime(
+            String name,
+            int value
+    ){
+    }
+}
