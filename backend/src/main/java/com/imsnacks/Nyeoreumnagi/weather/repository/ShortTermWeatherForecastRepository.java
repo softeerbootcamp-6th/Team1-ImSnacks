e2897a5ba3642c0a8d7b889570a733332f2a1466
imscow11253 <seaweed8759@naver.com>
2025-08-07 21:33:12 +0900

    feat: nx, ny로 단기기상예보 엔티티 조회하는 repository 메소드 생서

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/ShortTermWeatherForecastRepository.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/ShortTermWeatherForecastRepository.java
@@ -4,5 +4,8 @@ import com.imsnacks.Nyeoreumnagi.weather.entity.ShortTermWeatherForecast;
 import com.imsnacks.Nyeoreumnagi.weather.entity.ShortTermWeatherForecastId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShortTermWeatherForecastRepository extends JpaRepository<ShortTermWeatherForecast, ShortTermWeatherForecastId> {
+    List<ShortTermWeatherForecast> findAllByNxAndNy(int nx, int ny);
 }
