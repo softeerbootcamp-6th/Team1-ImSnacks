92adb782ede86c6f613c5c86f1aac3d5c249f547
imscow11253 <seaweed8759@naver.com>
2025-08-08 20:27:50 +0900

    feat: 기상 리스크 entity를 위한 jpa repository 인터페이스 생성

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/WeatherRiskRepository.java
@@ -0,0 +1,7 @@
+package com.imsnacks.Nyeoreumnagi.weather.repository;
+
+import com.imsnacks.Nyeoreumnagi.weather.entity.WeatherRisk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeatherRiskRepository extends JpaRepository<WeatherRisk, Long> {
+}
