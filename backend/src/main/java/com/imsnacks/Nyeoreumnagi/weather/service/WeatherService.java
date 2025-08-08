a3ef7b6c69be7d7db53907503482210976e03d65
imscow11253 <seaweed8759@naver.com>
2025-08-08 12:05:32 +0900

    feat: 기상 그래프 API를 위한 비즈니스 로직 implement

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/service/WeatherService.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/service/WeatherService.java
@@ -1,4 +1,4 @@
-package com.imsnacks.Nyeoreumnagi.weather;
+package com.imsnacks.Nyeoreumnagi.weather.service;
 
 import com.imsnacks.Nyeoreumnagi.common.enums.WeatherMetric;
 import com.imsnacks.Nyeoreumnagi.member.entity.Farm;
@@ -6,9 +6,18 @@ import com.imsnacks.Nyeoreumnagi.member.entity.Member;
 import com.imsnacks.Nyeoreumnagi.member.exception.MemberException;
 import com.imsnacks.Nyeoreumnagi.member.repository.MemberRepository;
 import com.imsnacks.Nyeoreumnagi.weather.dto.response.GetWeatherGraphResponse;
+import com.imsnacks.Nyeoreumnagi.weather.entity.ShortTermWeatherForecast;
+import com.imsnacks.Nyeoreumnagi.weather.exception.WeatherException;
 import com.imsnacks.Nyeoreumnagi.weather.repository.ShortTermWeatherForecastRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+
+import static com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus.INVALID_MEMBER_ID;
+import static com.imsnacks.Nyeoreumnagi.weather.exception.WeatherResponseStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +27,73 @@ public class WeatherService {
     private final ShortTermWeatherForecastRepository shortTermWeatherForecastRepository;
 
     public GetWeatherGraphResponse getWeatherGraph(Long memberId, WeatherMetric weatherMetric){
-        Member member = memberRepository.findById(memberId).orElseThrow(new MemberException());
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberException(INVALID_MEMBER_ID));
+        Farm farm = member.getFarm();
 
+        int nx = farm.getNx();
+        int ny = farm.getNy();
+
+        List<ShortTermWeatherForecast> weatherInfos = shortTermWeatherForecastRepository.findAllByNxAndNy(nx, ny);
+        if(weatherInfos.isEmpty()){
+            throw new WeatherException(NO_WEATHER_LOCATION);
+        }
+
+        int maxValue = getMaxValue(weatherInfos, weatherMetric);
+        int minValue = getMinValue(weatherInfos, weatherMetric);
+        List<GetWeatherGraphResponse.ValuePerTime> valuePerTimes = extractWeatherGraphInfos(weatherInfos, weatherMetric, LocalDateTime.now().getHour()+1);
+
+        int maxLimit = getUpperLimit(maxValue);
+        int minLimit = getLowerLimit(minValue, weatherMetric);
+
+        return new GetWeatherGraphResponse(maxLimit, minLimit, weatherMetric, valuePerTimes);
+    }
+
+    private List<GetWeatherGraphResponse.ValuePerTime> extractWeatherGraphInfos(List<ShortTermWeatherForecast> forecasts, WeatherMetric metric, int currentHour24) {
+        return forecasts.stream()
+                .map(f -> {
+                    String name = String.format("%02d", f.getFcstTime());
+                    int value = (int) getValue(f, metric);
+                    return new GetWeatherGraphResponse.ValuePerTime(name, value);
+                })
+                .sorted(Comparator.comparingInt(v -> {
+                    int hour = Integer.parseInt(v.name());
+                    return (hour - currentHour24 + 24) % 24;
+                }))
+                .toList();
+    }
+
+    private int getMaxValue(List<ShortTermWeatherForecast> forecasts, WeatherMetric metric) {
+        return forecasts.stream()
+                .mapToInt(f -> (int) getValue(f, metric))
+                .max()
+                .orElseThrow(() -> new WeatherException(NO_WEATHER_VALUE));
+    }
+
+    private int getMinValue(List<ShortTermWeatherForecast> forecasts, WeatherMetric metric) {
+        return forecasts.stream()
+                .mapToInt(f -> (int) getValue(f, metric))
+                .min()
+                .orElseThrow(() -> new WeatherException(NO_WEATHER_VALUE));
+    }
+
+    private double getValue(ShortTermWeatherForecast forecast, WeatherMetric metric) {
+        switch (metric) {
+            case PERCIPITATION: return forecast.getPrecipitation();
+            case TEMPERATURE: return forecast.getTemperature();
+            case HUMIDITY: return forecast.getHumidity();
+            case WIND_SPEED: return forecast.getWindSpeed();
+            default: throw new WeatherException(INVALID_WEATHER_METRIC);
+        }
+    }
+
+    private int getLowerLimit(int value, WeatherMetric metric){
+        if(metric.equals(WeatherMetric.TEMPERATURE)){
+            return ((value - 1) / 5) * 5;
+        }
+        return (value == 0) ? 0 : ((value - 1) / 5) * 5;
+    }
+
+    private int getUpperLimit(int value){
+        return ((value / 5) + 1) * 5;
     }
 }
