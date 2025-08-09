0ac2b97a7cb2e573f8c24385e72786744dbf68d0
imscow11253 <seaweed8759@naver.com>
2025-08-09 22:23:33 +0900

    feat: nx, ny가 같고 job execution id가 같은 WeatherRisk 반환하는 JPQL 쿼리 생성

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/WeatherRiskRepository.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/WeatherRiskRepository.java
@@ -2,6 +2,22 @@ package com.imsnacks.Nyeoreumnagi.weather.repository;
 
 import com.imsnacks.Nyeoreumnagi.weather.entity.WeatherRisk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WeatherRiskRepository extends JpaRepository<WeatherRisk, Long> {
+    @Query("""
+        SELECT wr
+        FROM WeatherRisk wr
+        WHERE wr.nx = :nx
+          AND wr.ny = :ny
+          AND wr.jobExecutionId = (
+            SELECT MAX(swr.jobExecutionId)
+            FROM WeatherRisk swr
+            WHERE swr.nx = :nx AND swr.ny = :ny
+          )
+    """)
+    List<WeatherRisk> findByNxAndNyWithMaxJobExecutionId(@Param("nx") int nx, @Param("ny") int ny);
 }
