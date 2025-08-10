2f3a6a851f271a629da43c16a840617efa5b6b66
imscow11253 <seaweed8759@naver.com>
2025-08-10 14:41:31 +0900

    refactor: 기상 리스크 우선순위에 따라 WeatherRisk enum 선언 순서 변경

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/enums/WeatherRiskType.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/enums/WeatherRiskType.java
@@ -1,12 +1,14 @@
 package com.imsnacks.Nyeoreumnagi.common.enums;
 
 public enum WeatherRiskType {
+    //날씨 특보 우선순위가 반영된 상태이니 수정시 논의가 필요
     RAIN("비"),
-    HEAVY_RAIN("호우"),
-    TORRENTIAL_RAIN("폭우"),
     FROST("서리"),
     ABNORMAL_HEAT("이상고온"),
-    STRONG_WIND("강풍");
+    HEAVY_RAIN("호우"),
+    STRONG_WIND("강풍"),
+    TORRENTIAL_RAIN("폭우"),
+    ;
 
     private final String description;
 
