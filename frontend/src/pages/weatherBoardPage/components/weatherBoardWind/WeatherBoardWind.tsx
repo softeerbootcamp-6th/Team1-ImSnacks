73f4c23a498cd8f350d7f2a9a9a6924b078f0811
bamb14 <118671229+bamb14@users.noreply.github.com>
2025-08-10 20:03:42 +0900

    feat: WeatherBoardWind 컴포넌트 개발

--- frontend/src/pages/weatherBoardPage/components/weatherBoardWind/WeatherBoardWind.tsx
+++ frontend/src/pages/weatherBoardPage/components/weatherBoardWind/WeatherBoardWind.tsx
@@ -1,12 +1,41 @@
-import { CommonStyles } from '@/styles/commonStyles';
+import S from './WeatherBoardWind.style';
+import { Typography } from '@/styles/typography';
+import WindArrow from '@/assets/icons/flat/WindArrow.svg?react';
+import { GrayScale } from '@/styles/colors';
 
-const WeatherBoardWind = () => {
+interface WeatherBoardWindProps {
+  direction: string;
+  speed: number;
+  degree: number;
+}
+
+const WeatherBoardWind = ({
+  direction,
+  speed,
+  degree,
+}: WeatherBoardWindProps) => {
   return (
-    <div css={CommonStyles.weatherBoardContainer}>
-      <div>
-        <h3>최고 풍속</h3>
-        <p>남서풍 10m/s</p>
+    <div css={S.WeatherBoardWind}>
+      <span css={Typography.Body_S_400}>N</span>
+      <div css={S.WeatherBoardWindContent}>
+        <svg width="52" height="52" viewBox="0 0 52 52" fill="none">
+          <circle
+            cx="26"
+            cy="26"
+            r="25"
+            stroke={GrayScale.G800}
+            strokeWidth="2"
+            opacity="0.5"
+            fill="none"
+          />
+        </svg>
+
+        <WindArrow width="48" height="54" css={S.WindArrow(degree)} />
       </div>
+      <h3>최고 풍속</h3>
+      <p>
+        {direction} • {speed}m/s
+      </p>
     </div>
   );
 };
