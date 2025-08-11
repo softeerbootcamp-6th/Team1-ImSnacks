2e09bec1594c8f4bf0fe60ae6957f72e407031da
imscow11253 <seaweed8759@naver.com>
2025-08-11 17:17:26 +0900

    update: WeatherCondition 계산 안되는 errer status code 추가

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/exception/WeatherResponseStatus.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/exception/WeatherResponseStatus.java
@@ -4,6 +4,7 @@ public enum WeatherResponseStatus {
     NO_WEATHER_LOCATION(3001, "해당 좌표에 날씨 정보가 없습니다."),
     INVALID_WEATHER_METRIC(3002, "유효하지 않은 weather metric입니다."),
     NO_WEATHER_VALUE(3003, "해당 지역에 날씨 데이터가 없습니다."),
+    CANNOT_CALCULATE_WEATHER_CONDITION(3004, "기상 요약을 할 수 없습니다."),
     ;
 
     private final int code;
