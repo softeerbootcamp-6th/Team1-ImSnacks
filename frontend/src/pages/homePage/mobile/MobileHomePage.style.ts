1ac66d7cbc09fe84064ed81ac5eaf882fdf4878a
bamb14 <althcjstk08@gmail.com>
2025-08-11 18:45:40 +0900

    feat: MobileHomePage 컴포넌트 분리 구현 중

---
+++ frontend/src/pages/homePage/mobile/MobileHomePage.style.ts
@@ -0,0 +1,11 @@
+import { FlexStyles } from '@/styles/commonStyles';
+import { css } from '@emotion/react';
+
+const MobileHomePage = css`
+  ${FlexStyles.flexColumn};
+  margin-top: 32px;
+`;
+
+export default {
+  MobileHomePage,
+};
---
+++ frontend/src/pages/homePage/mobile/MobileHomePage.tsx
@@ -0,0 +1,12 @@
+import HeadLine from '../components/headLine/HeadLine';
+import S from './MobileHomePage.style';
+
+const MobileHomePage = () => {
+  return (
+    <div css={S.MobileHomePage}>
+      <HeadLine />
+    </div>
+  );
+};
+
+export default MobileHomePage;
