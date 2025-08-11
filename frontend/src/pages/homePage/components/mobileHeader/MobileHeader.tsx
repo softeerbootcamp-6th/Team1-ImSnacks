10cd7ba3c42e0c6718e43e7f309699c3a26522a3
bamb14 <althcjstk08@gmail.com>
2025-08-11 18:46:02 +0900

    feat: MobileHeader 컴포넌트 구현 중

---
+++ frontend/src/pages/homePage/components/mobileHeader/MobileHeader.tsx
@@ -0,0 +1,16 @@
+const MobileHeader = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        padding: '0 20px',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        alignSelf: 'stretch',
+      }}
+    ></div>
+  );
+};
+
+export default MobileHeader;
