5a474907e526f9935fc6f8875482e3ca49385b8d
bamb14 <118671229+bamb14@users.noreply.github.com>
2025-08-12 01:13:14 +0900

    feat: GLASS_ICON, GLASS_MOBILE_ICON constants 파일 추가

---
+++ frontend/src/constants/glassMobileIcon.ts
@@ -0,0 +1,28 @@
+import type { WeatherConditionsType } from '@/types/weather.types';
+
+import {
+  SunnyMobileIcon,
+  SnowMobileIcon,
+  WindMobileIcon,
+  CloudyMobileIcon,
+  CloudyNightMobileIcon,
+  HeavyRainMobileIcon,
+  LessCloudyMobileIcon,
+  NightMobileIcon,
+  RainMobileIcon,
+} from '@/assets/icons/glass/mobileSize';
+
+export const GLASS_MOBILE_ICON: Record<
+  WeatherConditionsType | string,
+  React.FC<React.SVGProps<SVGSVGElement>>
+> = {
+  SUNNY: SunnyMobileIcon,
+  SNOW: SnowMobileIcon,
+  STRONG_WIND: WindMobileIcon,
+  CLOUDY: CloudyMobileIcon,
+  CLOUDY_NIGHT: CloudyNightMobileIcon,
+  HEAVY_RAIN: HeavyRainMobileIcon,
+  LESS_CLOUDY: LessCloudyMobileIcon,
+  NIGHT: NightMobileIcon,
+  RAIN: RainMobileIcon,
+};
