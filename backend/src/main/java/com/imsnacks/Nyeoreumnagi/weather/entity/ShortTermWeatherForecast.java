458623cc9eecc1b02a8b624f1a295f875114534b
imscow11253 <seaweed8759@naver.com>
2025-08-11 17:18:27 +0900

    feat: 단기 기상 예보 entity 클래스에 WeatherCondition 반환하는 메소드 구현

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/entity/ShortTermWeatherForecast.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/entity/ShortTermWeatherForecast.java
@@ -1,5 +1,9 @@
 package com.imsnacks.Nyeoreumnagi.weather.entity;
 
+import com.imsnacks.Nyeoreumnagi.common.enums.WeatherCondition;
+import com.imsnacks.Nyeoreumnagi.weather.exception.WeatherException;
+import com.imsnacks.Nyeoreumnagi.weather.exception.WeatherResponseStatus;
+import com.imsnacks.Nyeoreumnagi.weather.service.SunriseSunSetTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
@@ -10,6 +14,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Builder;
+
+import static com.imsnacks.Nyeoreumnagi.weather.exception.WeatherResponseStatus.CANNOT_CALCULATE_WEATHER_CONDITION;
 
 @Entity
 @Table(name = "ShortTermWeatherForecast")
@@ -46,4 +52,21 @@ public class ShortTermWeatherForecast {
 
     @Column(name = "sky_status", nullable = false)
     private int skyStatus;
+
+    public WeatherCondition getWeatherCondition(SunriseSunSetTime times){
+        if(snow > 1) return WeatherCondition.SNOW;
+        if(precipitation >= 30) return WeatherCondition.HEAVY_RAIN;
+        if(precipitation > 0) return WeatherCondition.RAIN;
+        if(windSpeed >= 14) return WeatherCondition.STRONG_WIND;
+        if(fcstTime >= times.getSunriseTime().getHour() && fcstTime < times.getSunSetTime().getHour()){
+            if(skyStatus == 1) return WeatherCondition.SUNNY;
+            if(skyStatus == 3) return WeatherCondition.LESS_CLOUDY;
+            if(skyStatus == 4) return WeatherCondition.CLOUDY;
+        }
+        else{
+            if(skyStatus == 1) return WeatherCondition.NIGHT;
+            if(skyStatus == 3 || skyStatus == 4) return WeatherCondition.CLOUDY_NIGHT;
+        }
+        throw new WeatherException(CANNOT_CALCULATE_WEATHER_CONDITION);
+    }
 }
