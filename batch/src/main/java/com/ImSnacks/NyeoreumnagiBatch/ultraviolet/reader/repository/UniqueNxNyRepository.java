8fa8b95520e75ea65c53aea12207215fe8d5c519
imscow11253 <seaweed8759@naver.com>
2025-08-12 14:17:03 +0900

    refactor: projection 쿼리를 직접 작성하는 것으로 변경

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/reader/repository/UniqueNxNyRepository.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/reader/repository/UniqueNxNyRepository.java
@@ -4,9 +4,11 @@ import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.entity.NxN
 import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.entity.UniqueNxNy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UniqueNxNyRepository extends JpaRepository<UniqueNxNy, NxNyId> {
     @Query("SELECT up.areaCode FROM UniqueNxNy up")
     List<String> findAreaCodes();
