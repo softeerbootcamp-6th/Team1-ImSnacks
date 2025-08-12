b93ed3076986b3244912ea44805f5887a4435172
imscow11253 <seaweed8759@naver.com>
2025-08-12 16:14:58 +0900

    refactor: 자외선 batch processor의 dto에 nx, ny 필드 추가

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/processor/dto/UVProcessorResponseDto.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/processor/dto/UVProcessorResponseDto.java
@@ -3,6 +3,8 @@ package com.ImSnacks.NyeoreumnagiBatch.ultraviolet.processor.dto;
 import java.time.LocalTime;
 
 public record UVProcessorResponseDto(
+        int nx,
+        int ny,
         int maxUVIndex,
         LocalTime maxUVStartTime,
         LocalTime maxUVEndTime
