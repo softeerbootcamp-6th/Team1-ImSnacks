f9e718230cf29a8dc967cda1188d15c04633fa8b
imscow11253 <seaweed8759@naver.com>
2025-08-12 12:22:55 +0900

    feat: 자외선 open api를 호출하는 UVApiCaller 클래스 생성

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/reader/UVApiCaller.java
@@ -0,0 +1,42 @@
+package com.ImSnacks.NyeoreumnagiBatch.ultraviolet.reader;
+
+import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.dto.VilageFcstResponseDto;
+import com.ImSnacks.NyeoreumnagiBatch.ultraviolet.reader.dto.UVReaderResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static com.ImSnacks.NyeoreumnagiBatch.ultraviolet.reader.enums.UVApiRequestValue.*;
+
+@Component
+public class UVApiCaller {
+    @Value("${api.service.uv-key}")
+    private String secretKey;
+
+    public UVReaderResponseDto call(int areaCode, String time){
+        RestClient restClient = RestClient.create();
+        String uriString = buildUriString(areaCode, time);
+
+        return restClient.get()
+                .uri(uriString)
+                .retrieve()
+                .body(UVReaderResponseDto.class);
+    }
+
+    private String buildUriString(int areaCode, String time){
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(UV_URL.toString());
+
+        UriComponents uri = builder
+                .queryParam(SERVICE_KEY.toString(), secretKey)
+                .queryParam(PAGE_NUM.toString(), 1)
+                .queryParam(NUM_OF_ROWS.toString(), 10)
+                .queryParam(DATA_TYPE.toString(), "JSON")
+                .queryParam(AREA_NO.toString(), areaCode)
+                .queryParam(TIME.toString(), time)
+                .build();
+
+        return uri.encode().toUriString();
+    }
+}
