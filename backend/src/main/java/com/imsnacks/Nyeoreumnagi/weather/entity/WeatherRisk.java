f8743edd552e76aabe29b347949f9311aede7915
lim <gyuhyun.lim@outlook.com>
2025-08-12 11:17:45 +0900

    chore: 중복된 jobExecutionId 필드 제거

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/entity/WeatherRisk.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/entity/WeatherRisk.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -19,7 +20,6 @@ public class WeatherRisk {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long weatherRiskId;
-    private Long jobExecutionId;
     private LocalDate fcstDate; //지워야 함
     private LocalDateTime startTime;
     private LocalDateTime endTime;
