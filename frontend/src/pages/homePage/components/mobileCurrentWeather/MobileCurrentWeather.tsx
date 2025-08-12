f8d8aa8ab71d0d3d94966b2acef660bc8074c172
bamb14 <althcjstk08@gmail.com>
2025-08-12 12:02:55 +0900

    feat: MobileCurrentWeather 컴포넌트 구현 및 스타일링

---
+++ frontend/src/pages/homePage/components/mobileCurrentWeather/MobileCurrentWeather.tsx
@@ -0,0 +1,38 @@
+import type { WeatherMetrics } from '@/types/weather.types';
+import S from './MobileCurrentWeather.style';
+import { Typography } from '@/styles/typography';
+import MobileCurrentWeatherContent from '../mobileCurrentWeatherContent/MobileCurrentWeatherContent';
+
+interface MobileCurrentWeatherProps {
+  metric: string;
+  metricType: WeatherMetrics;
+  value: number;
+}
+
+interface MobileCurrentWeatherComponentProps {
+  weatherData: MobileCurrentWeatherProps[];
+}
+
+const MobileCurrentWeather = ({
+  weatherData,
+}: MobileCurrentWeatherComponentProps) => {
+  return (
+    <div css={S.MobileCurrentWeather}>
+      <h2 css={Typography.Mobile_Headline}>현 시각 기상 상황</h2>
+      <div css={S.MobileCurrentWeatherContentWrapper}>
+        <div css={S.MobileCurrentWeatherContentRow}>
+          {weatherData.slice(0, 2).map((item: MobileCurrentWeatherProps) => (
+            <MobileCurrentWeatherContent data={item} />
+          ))}
+        </div>
+        <div css={S.MobileCurrentWeatherContentRow}>
+          {weatherData.slice(2).map((item: MobileCurrentWeatherProps) => (
+            <MobileCurrentWeatherContent data={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileCurrentWeather;
