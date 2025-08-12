bc13480d9eb67a0e8ee74d2c471dd69d8e5bcff4
imscow11253 <seaweed8759@naver.com>
2025-08-12 16:14:30 +0900

    refactor: 자외선 batch processor에서 nx, ny도 dto에 담아서 반환하도록 수정

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/processor/UVProcessor.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/processor/UVProcessor.java
@@ -3,6 +3,7 @@ package com.ImSnacks.NyeoreumnagiBatch.ultraviolet.processor;
 import com.ImSnacks.NyeoreumnagiBatch.ultraviolet.exception.UVProcessorException;
 import com.ImSnacks.NyeoreumnagiBatch.ultraviolet.processor.dto.UVProcessorResponseDto;
 import com.ImSnacks.NyeoreumnagiBatch.ultraviolet.reader.dto.UVReaderResponseDto;
+import com.ImSnacks.NyeoreumnagiBatch.ultraviolet.reader.dto.UVReaderResponseDtoWithNxNy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
@@ -11,17 +12,19 @@ import java.time.LocalTime;
 
 @Slf4j
 @Component
-public class UVProcessor implements ItemProcessor<UVReaderResponseDto, UVProcessorResponseDto> {
+public class UVProcessor implements ItemProcessor<UVReaderResponseDtoWithNxNy, UVProcessorResponseDto> {
 
     @Override
-    public UVProcessorResponseDto process(UVReaderResponseDto item) throws Exception {
+    public UVProcessorResponseDto process(UVReaderResponseDtoWithNxNy item) throws Exception {
         log.info("Processing UV Processor");
-        UVReaderResponseDto.Item uvInfos = item.getItems();
+        UVReaderResponseDto.Item uvInfos = item.getDto().getItems();
+        int nx = item.getNx();
+        int ny = item.getNy();
 
-        return getMaxValue(uvInfos);
+        return getMaxValue(uvInfos, nx, ny);
     }
 
-    private UVProcessorResponseDto getMaxValue(UVReaderResponseDto.Item unInfos){
+    private UVProcessorResponseDto getMaxValue(UVReaderResponseDto.Item unInfos, int nx, int ny) throws UVProcessorException {
         int maxValue = 0;
         LocalTime startTime = null;
         LocalTime endTime = null;
@@ -40,7 +43,7 @@ public class UVProcessor implements ItemProcessor<UVReaderResponseDto, UVProcess
             throw new UVProcessorException("자외선의 최대값을 걔산할 수 없습니다.");
         }
 
-        return new UVProcessorResponseDto(maxValue, startTime, endTime);
+        return new UVProcessorResponseDto(nx, ny, maxValue, startTime, endTime);
     }
 
     private Integer getValue(UVApiField field, UVReaderResponseDto.Item unInfos){
