0df3357eb09e3e4a4d1b02c8f665df54db31426b
bamb14 <althcjstk08@gmail.com>
2025-08-12 15:28:00 +0900

    feat: MobileHomePage UI 구현 및 스타일링

--- frontend/src/pages/homePage/mobile/MobileHomePage.style.ts
+++ frontend/src/pages/homePage/mobile/MobileHomePage.style.ts
@@ -1,11 +1,14 @@
+import { GrayScale } from '@/styles/colors';
 import { FlexStyles } from '@/styles/commonStyles';
 import { Spacing } from '@/styles/spacing';
+import { Typography } from '@/styles/typography';
 import { css } from '@emotion/react';
 
 const MobileHomePage = css`
   ${FlexStyles.flexColumn};
   width: 100%;
   margin-top: 87px;
+  margin-bottom: 32px;
 `;
 
 const MobileHomeContentWrapper = css`
@@ -14,6 +17,12 @@ const MobileHomeContentWrapper = css`
   align-items: flex-start;
   gap: ${Spacing.Spacing1100};
   align-self: stretch;
+  padding: 0 ${Spacing.Spacing600};
+
+  h2 {
+    ${Typography.Mobile_Headline};
+    color: ${GrayScale.White};
+  }
 `;
 
 export default {
