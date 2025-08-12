01c825b2e76f8e8dc3d3413e72d16bb21816a9f2
lim <gyuhyun.lim@outlook.com>
2025-08-12 16:56:43 +0900

    chore: 브리핑 테스트 제거

--- backend/src/test/java/com/imsnacks/Nyeoreumnagi/weather/WeatherServiceTest.java
+++ backend/src/test/java/com/imsnacks/Nyeoreumnagi/weather/WeatherServiceTest.java
@@ -6,10 +6,8 @@ import com.imsnacks.Nyeoreumnagi.common.enums.WeatherRiskType;
 import com.imsnacks.Nyeoreumnagi.member.entity.Farm;
 import com.imsnacks.Nyeoreumnagi.member.entity.Member;
 import com.imsnacks.Nyeoreumnagi.member.exception.MemberException;
-import com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus;
 import com.imsnacks.Nyeoreumnagi.member.repository.MemberRepository;
 import com.imsnacks.Nyeoreumnagi.weather.dto.response.GetFcstRiskResponse;
-import com.imsnacks.Nyeoreumnagi.weather.dto.response.GetWeatherBriefingResponse;
 import com.imsnacks.Nyeoreumnagi.weather.dto.response.GetWeatherConditionResponse;
 import com.imsnacks.Nyeoreumnagi.weather.dto.response.GetWeatherGraphResponse;
 import com.imsnacks.Nyeoreumnagi.weather.entity.ShortTermWeatherForecast;
@@ -20,7 +18,6 @@ import com.imsnacks.Nyeoreumnagi.weather.repository.ShortTermWeatherForecastRepo
 import com.imsnacks.Nyeoreumnagi.weather.repository.WeatherRiskRepository;
 import com.imsnacks.Nyeoreumnagi.weather.service.SunriseSunSetTime;
 import com.imsnacks.Nyeoreumnagi.weather.service.WeatherService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,8 +30,6 @@ import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -42,7 +37,6 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
@@ -168,101 +162,6 @@ class WeatherServiceTest {
         assertThat(risks.get(3).endTime()).isEqualTo("8");
     }
 
-    @Test
-    void WeatherBriefing_유효하지_않은_멤버_입력시_예외_발생() {
-        final Long wrongMemberId = 42L;
-        assertThatThrownBy(() -> weatherService.getWeatherBriefing(wrongMemberId)).isInstanceOf(MemberException.class);
-        try {
-            weatherService.getWeatherBriefing(wrongMemberId);
-        } catch (MemberException e) {
-            assertThat(e.getStatus()).isEqualTo(MemberResponseStatus.MEMBER_NOT_FOUND);
-        }
-    }
-
-    @Test
-    void WeatherBriefing_농장이_없는_경우_예외_발생() {
-        final long memberId = 42L;
-        final Member member = new Member(memberId, "", "", "", "", null, null);
-        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
-
-//        assertThatThrownBy(() -> weatherService.getWeatherBriefing(memberId))
-//                .isInstanceOf(MemberException.class)
-//                .extracting("status")
-//                .isEqualTo(MemberResponseStatus.NO_FARM_INFO);
-        Assertions.assertThrows(MemberException.class, () -> {
-            weatherService.getWeatherBriefing(memberId);
-        });
-    }
-
-    @Test
-    void WeatherBriefing_날씨특보_정보가_없는_경우_false와_빈문자열_반환() {
-        // given
-        final long memberId = 42L;
-        final Farm farm = new Farm(memberId, "", "", "", "", 36.12, 127.12, 60, 120, null);
-        final Member member = new Member(memberId, "", "", "", "", null, farm);
-        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
-
-        // when
-        GetWeatherBriefingResponse actual = weatherService.getWeatherBriefing(memberId);
-
-        // then
-        GetWeatherBriefingResponse expected = new GetWeatherBriefingResponse(false, "");
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    void WeatherBriefing_날씨특보_하나만_있을때_출력() {
-        final long memberId = 42L;
-        final int nx = 60;
-        final int ny = 120;
-        final Farm farm = new Farm(memberId, "", "", "", "", 36.12, 127.12, nx, ny, null);
-        final Member member = new Member(memberId, "", "", "", "", null, farm);
-        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
-
-        final long riskId = 1L;
-        final long jobExecutionId = 1L;
-        final LocalDate fcstDate = LocalDate.now();
-        final LocalDateTime from = LocalDateTime.now();
-        final LocalDateTime to = LocalDateTime.now().plusHours(2);
-
-        for (WeatherRiskType type : WeatherRiskType.values()) {
-            WeatherRisk risk = new WeatherRisk(riskId, jobExecutionId, fcstDate, from, to, nx, ny, type);
-            //given(weatherRiskRepository.findById(riskId)).willReturn(Optional.of(risk));
-            when(weatherRiskRepository.findByNxAndNyWithMaxJobExecutionId(60, 120))
-                    .thenReturn(List.of(risk));
-            GetWeatherBriefingResponse actual = weatherService.getWeatherBriefing(memberId);
-            log.info(actual.toString());
-        }
-    }
-
-    @Test
-    void WeatherBriefing_날씨특보_여러개가_같은_시간대일때_우선순위_확인() {
-        final long memberId = 42L;
-        final int nx = 60;
-        final int ny = 120;
-        final Farm farm = new Farm(memberId, "", "", "", "", 36.12, 127.12, nx, ny, null);
-        final Member member = new Member(memberId, "", "", "", "", null, farm);
-        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
-
-        final long jobExecutionId = 1L;
-        final LocalDate fcstDate = LocalDate.now();
-        final LocalDateTime from = LocalDateTime.now();
-        final LocalDateTime to = LocalDateTime.now().plusHours(2);
-
-        // 동일 기간의 리스크 여러개로 리스트를 구성한다.
-        // 가장 우선순위가 높은 "폭우" 정보가 나와야 한다.
-        List<WeatherRisk> risks = new ArrayList<>();
-        long riskId = 1L;
-        for (WeatherRiskType type : WeatherRiskType.values()) {
-            WeatherRisk e = new WeatherRisk(riskId++, jobExecutionId, fcstDate, from, to, nx, ny, type);
-            risks.add(e);
-        }
-        when(weatherRiskRepository.findByNxAndNyWithMaxJobExecutionId(nx, ny)).thenReturn(risks);
-
-        GetWeatherBriefingResponse actual = weatherService.getWeatherBriefing(memberId);
-        log.info(actual.toString());
-    }
-  
     void 정상_날씨_조회_성공() {
         // given
         long memberId = 1L;
