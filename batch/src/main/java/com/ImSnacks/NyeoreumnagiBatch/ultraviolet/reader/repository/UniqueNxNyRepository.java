d2b1284607e0605394bfb454f0cef12916d4d03f
imscow11253 <seaweed8759@naver.com>
2025-08-12 14:04:55 +0900

    feat: nx, ny 정보를 가져오기 위한 jpa repository 생성

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/ultraviolet/reader/repository/UniqueNxNyRepository.java
@@ -0,0 +1,13 @@
+package com.ImSnacks.NyeoreumnagiBatch.ultraviolet.reader.repository;
+
+import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.entity.NxNyId;
+import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.entity.UniqueNxNy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UniqueNxNyRepository extends JpaRepository<UniqueNxNy, NxNyId> {
+    @Query("SELECT up.areaCode FROM UniqueNxNy up")
+    List<String> findAreaCodes();
+}
