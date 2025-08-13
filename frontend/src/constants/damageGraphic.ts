e6e5d346a7523dbbecc97082563271bf0e62ada5
bamb14 <althcjstk08@gmail.com>
2025-08-13 21:52:16 +0900

    chore: damage 아이콘 index.ts 적용

--- frontend/src/constants/damageGraphic.ts
+++ frontend/src/constants/damageGraphic.ts
@@ -1,10 +1,13 @@
 import type { WeatherDamagesType, PestDamagesType } from '@/types/damage.type';
-import FlowerDrop from '@/assets/images/weatherDemage/FlowerDrop.svg?react';
-import FruitCraking from '@/assets/images/weatherDemage/FruitCraking.svg?react';
-import Sunburn from '@/assets/images/weatherDemage/SunBrun.svg?react';
-import FruitDrop from '@/assets/images/weatherDemage/FruitDrop.svg?react';
-import PoorColoring from '@/assets/images/weatherDemage/PoorColoring.svg?react';
-import WaterDefficiency from '@/assets/images/weatherDemage/WaterDefficieny.svg?react';
+
+import {
+  FlowerDrop,
+  FruitCraking,
+  SunBurn,
+  FruitDrop,
+  PoorColoring,
+  WaterDefficiency,
+} from '@/assets/images/weatherDemage';
 
 import Bugs from '@/assets/images/pestDemage/Bugs.svg?react';
 import Germs from '@/assets/images/pestDemage/Germs.svg?react';
@@ -15,7 +18,7 @@ export const DAMAGE_GRAPHIC: Record<
 > = {
   FLOWER_DROP: FlowerDrop,
   FRUIT_CRAKING: FruitCraking,
-  SUNBURN: Sunburn,
+  SUNBURN: SunBurn,
   FRUIT_DROP: FruitDrop,
   POOR_COLORING: PoorColoring,
   WATER_DEFFICIENCY: WaterDefficiency,
