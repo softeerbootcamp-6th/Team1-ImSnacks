c218d5f97a4fa395a24ff4f975340f7ed435a380
imscow11253 <seaweed8759@naver.com>
2025-08-13 20:36:32 +0900

    feat: 일출몰 batch processor 로직 구현

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/sunRiseSet/processor/SunRiseSetProcessor.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/sunRiseSet/processor/SunRiseSetProcessor.java
@@ -2,11 +2,30 @@ package com.ImSnacks.NyeoreumnagiBatch.sunRiseSet.processor;
 
 import com.ImSnacks.NyeoreumnagiBatch.sunRiseSet.processor.dto.SunRiseSetProcessorResponseDto;
 import com.ImSnacks.NyeoreumnagiBatch.sunRiseSet.reader.dto.SunRiseSetReaderResponseDtoWithNxNy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Component
 public class SunRiseSetProcessor implements ItemProcessor<SunRiseSetReaderResponseDtoWithNxNy, SunRiseSetProcessorResponseDto> {
     @Override
     public SunRiseSetProcessorResponseDto process(SunRiseSetReaderResponseDtoWithNxNy item) throws Exception {
-        return null;
+        log.info("Processing SunRiseSet Processor");
+
+        LocalTime sunRiseTime = parseStringToLocalTime(item.dto().getSunRise());
+        LocalTime sunSetTime = parseStringToLocalTime(item.dto().getSunset());
+        int nx = item.nx();
+        int ny = item.ny();
+
+        return new SunRiseSetProcessorResponseDto(nx, ny, sunRiseTime, sunSetTime);
+    }
+
+    private LocalTime parseStringToLocalTime(String str) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmm");
+        return LocalTime.parse(str, formatter);
     }
 }
