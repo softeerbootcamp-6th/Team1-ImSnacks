83efeb6413be3e3ce90f917b911b92d4114b44ba
lim <gyuhyun.lim@outlook.com>
2025-08-13 21:10:05 +0900

    fix: 패키지 경로 변경

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/work/repository/LifeCycleRepository.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/work/repository/LifeCycleRepository.java
@@ -1,8 +1,7 @@
 package com.imsnacks.Nyeoreumnagi.work.repository;
 
-import com.imsnacks.Nyeoreumnagi.work.entity.LifeCycle;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.imsnacks.Nyeoreumnagi.lifecycle.entity.LifeCycle;
 import java.util.List;
 
 public interface LifeCycleRepository extends JpaRepository<LifeCycle, Integer> {
