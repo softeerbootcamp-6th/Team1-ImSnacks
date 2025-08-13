12ad80d69e8ed3eab4ec77b588f435e3cebdb1ce
imscow11253 <seaweed8759@naver.com>
2025-08-13 14:56:42 +0900

    refactor: 날씨 정보 테이블 entity 필드값 nullable 허용

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/entity/DashboardTodayWeather.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/entity/DashboardTodayWeather.java
@@ -19,45 +19,45 @@ public class DashboardTodayWeather {
     @Id
     private int ny;
 
-    @Column(name = "max_precipitation", nullable = false)
-    private double maxPrecipitation;
+    @Column(name = "max_precipitation", nullable = true)
+    private Double maxPrecipitation;
 
-    @Column(name = "max_humidity", nullable = false)
-    private double maxHumidity;
+    @Column(name = "max_humidity", nullable = true)
+    private Double maxHumidity;
 
-    @Column(name = "max_windspeed", nullable = false)
-    private double maxWindSpeed;
+    @Column(name = "max_windspeed", nullable = true)
+    private Double maxWindSpeed;
 
-    @Column(name = "wind_direction", nullable = false)
-    private double windDirection;
+    @Column(name = "wind_direction", nullable = true)
+    private Double windDirection;
 
-    @Column(name = "max_temperature", nullable = false)
-    private int maxTemperature;
+    @Column(name = "max_temperature", nullable = true)
+    private Integer maxTemperature;
 
-    @Column(name = "max_uv_index", nullable = false)
-    private int maxUVIndex;
+    @Column(name = "max_uv_index", nullable = true)
+    private Integer maxUVIndex;
 
-    @Column(name = "max_uv_start", nullable = false)
+    @Column(name = "max_uv_start", nullable = true)
     private LocalTime maxUVStart;
 
-    @Column(name = "max_uv_end", nullable = false)
+    @Column(name = "max_uv_end", nullable = true)
     private LocalTime maxUVEnd;
 
-    @Column(name = "pm_10_value", nullable = false)
-    private double pm10Value;
+    @Column(name = "pm_10_value", nullable = true)
+    private Double pm10Value;
 
-    @Column(name = "pm_10_grade", nullable = false)
-    private int pm10Grade;
+    @Column(name = "pm_10_grade", nullable = true)
+    private Integer pm10Grade;
 
-    @Column(name = "pm_25_value", nullable = false)
-    private double pm25Value;
+    @Column(name = "pm_25_value", nullable = true)
+    private Double pm25Value;
 
-    @Column(name = "pm_25_grade", nullable = false)
-    private int pm25Grade;
+    @Column(name = "pm_25_grade", nullable = true)
+    private Integer pm25Grade;
 
-    @Column(name = "sunrise_time", nullable = false)
+    @Column(name = "sunrise_time", nullable = true)
     private LocalTime sunriseTime;
 
-    @Column(name = "sunset_time", nullable = false)
+    @Column(name = "sunset_time", nullable = true)
     private LocalTime sunSetTime;
 }
