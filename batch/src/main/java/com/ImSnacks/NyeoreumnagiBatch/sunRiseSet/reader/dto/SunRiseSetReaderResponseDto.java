e5c10d8d563cadb933774245ca0dcefb0164c778
imscow11253 <seaweed8759@naver.com>
2025-08-13 19:48:41 +0900

    feat: 일출몰 response를 받기 위한 dto 정의

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/sunRiseSet/reader/dto/SunRiseSetReaderResponseDto.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/sunRiseSet/reader/dto/SunRiseSetReaderResponseDto.java
@@ -1,6 +1,58 @@
 package com.ImSnacks.NyeoreumnagiBatch.sunRiseSet.reader.dto;
 
-public record SunRiseSetReaderResponseDto (
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+import java.util.List;
+
+public record SunRiseSetReaderResponseDto (
+        Script[] script,
+        Header header,
+        Body body
 ){
+    public record Script() {} // <script/> 빈 태그
+
+    public record Header(
+            @JacksonXmlProperty(localName = "resultCode")
+            String resultCode,
+            @JacksonXmlProperty(localName = "resultMsg")
+            String resultMsg
+    ) {}
+
+    public record Body(
+            Items items,
+            @JacksonXmlProperty(localName = "numOfRows")
+            int numOfRows,
+            @JacksonXmlProperty(localName = "pageNo")
+            int pageNo,
+            @JacksonXmlProperty(localName = "totalCount")
+            int totalCount
+    ) {}
+
+    public record Items(
+            @JacksonXmlElementWrapper(useWrapping = false)
+            @JacksonXmlProperty(localName = "item")
+            List<Item> item
+    ) {}
+
+    public record Item(
+            String aste,
+            String astm,
+            String civile,
+            String civilm,
+            String latitude,
+            String latitudeNum,
+            String location,
+            String locdate,
+            String longitude,
+            String longitudeNum,
+            String moonrise,
+            String moonset,
+            String moontransit,
+            String naute,
+            String nautm,
+            String sunrise,
+            String sunset,
+            String suntransit
+    ) {}
 }
