91ca6f041906c603275cf0ef553527cff06ceee4
bamb14 <althcjstk08@gmail.com>
2025-08-13 20:38:23 +0900

    feat: DamageCard 컴포넌트 구현 및 스타일링

---
+++ frontend/src/pages/homePage/components/damageCard/DamageCard.style.ts
@@ -0,0 +1,35 @@
+import { Assets } from '@/styles/colors';
+import { Typography } from '@/styles/typography';
+import { css } from '@emotion/react';
+
+const DamageCard = (selectedName: string | null, name: string) => css`
+  width: 342px;
+  height: 218px;
+  position: relative;
+  backdrop-filter: blur(6px);
+  border-radius: 16px;
+  cursor: pointer;
+  opacity: ${selectedName && selectedName !== name ? 0.2 : 1};
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: ${selectedName !== name && 'rotate(10deg)'};
+  }
+`;
+
+const DamageCardTitle = css`
+  ${Typography.Subtitle_700}
+  color: ${Assets.Text.Global.Headline};
+`;
+
+const DamageCardDescription = css`
+  ${Typography.Body_S_400}
+  color: ${Assets.Text.Global.Body};
+  width: 70%;
+`;
+
+export default {
+  DamageCard,
+  DamageCardTitle,
+  DamageCardDescription,
+};
---
+++ frontend/src/pages/homePage/components/damageCard/DamageCard.tsx
@@ -0,0 +1,45 @@
+import { DAMAGE_GRAPHIC } from '@/constants/damageGraphic';
+import { Assets } from '@/styles/colors';
+import { Typography } from '@/styles/typography';
+import type { PestDamagesType, WeatherDamagesType } from '@/types/damage.type';
+import { css } from '@emotion/react';
+import S from './DamageCard.style';
+
+interface DamageCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  name: string;
+  description: string;
+  damageType: WeatherDamagesType | PestDamagesType;
+  selectedName?: string | null;
+}
+
+const DamageCard = ({
+  name,
+  description,
+  damageType,
+  selectedName = null,
+  ...props
+}: DamageCardProps) => {
+  const DamageGraphicComponent = DAMAGE_GRAPHIC[damageType];
+  const opacity = selectedName === name ? 0.9 : 0.6;
+
+  return (
+    <div css={S.DamageCard(selectedName, name)} {...props}>
+      <DamageGraphicComponent
+        width={342}
+        height={218}
+        style={
+          {
+            '--fill-opacity': opacity,
+          } as React.CSSProperties & { '--fill-opacity': number }
+        }
+      />
+      <div style={{ position: 'absolute', top: 110, left: 24 }}>
+        <div css={S.DamageCardTitle}>{name}</div>
+
+        <div css={S.DamageCardDescription}>{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default DamageCard;
