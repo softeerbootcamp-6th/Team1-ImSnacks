6537fdafa9f59dd0d08a4bb64146563c8eb18566
lim <gyuhyun.lim@outlook.com>
2025-08-15 01:32:58 +0900

    feat: PestRisk entity 추가

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/pest/entity/PestRisk.java
@@ -0,0 +1,36 @@
+package com.imsnacks.Nyeoreumnagi.pest.entity;
+
+import com.imsnacks.Nyeoreumnagi.work.entity.Crop;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access=lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name="PestRisk")
+@Entity
+public class PestRisk {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long pestRiskId;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @OneToMany()
+    private List<PestCondition> conditions;
+
+    @ManyToOne()
+    private Crop crop;
+}
