dfb49e319257c02d2297dafb455bef0de0c3a31f
imscow11253 <seaweed8759@naver.com>
2025-08-14 12:06:08 +0900

    feat: 자외선 정보 없을 때 error status 정의

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/exception/WeatherResponseStatus.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/exception/WeatherResponseStatus.java
@@ -5,6 +5,7 @@ public enum WeatherResponseStatus {
     INVALID_WEATHER_METRIC(3002, "유효하지 않은 weather metric입니다."),
     NO_WEATHER_VALUE(3003, "해당 지역에 날씨 데이터가 없습니다."),
     CANNOT_CALCULATE_WEATHER_CONDITION(3004, "기상 요약을 할 수 없습니다."),
+    NO_UV_INFO(3005, "해당 지역 자외선 정보가 없습니다."),
     ;
 
     private final int code;
