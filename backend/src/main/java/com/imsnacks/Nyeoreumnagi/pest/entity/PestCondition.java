911f24ded6037176c8154abc18e32612a992862d
lim <gyuhyun.lim@outlook.com>
2025-08-15 01:47:51 +0900

    feat: PestCondition entity 추가

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/pest/entity/PestCondition.java
@@ -0,0 +1,68 @@
+package com.imsnacks.Nyeoreumnagi.pest.entity;
+
+import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode.HumidityCode;
+import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode.RainCode;
+import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode.TemperatureCode;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Month;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PestCondition")
+@Entity
+public class PestCondition {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long pestConditionId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private PestRisk pestRisk;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "start_month", nullable = false)
+    private Month startMonth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "end_month", nullable = false)
+    private Month endMonth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "start_phase", nullable = false)
+    private MonthPhase startMonthPhase;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "end_phase", nullable = false)
+    private MonthPhase endMonthPhase;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "humidity_code", nullable = false)
+    private HumidityCode humidityCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "temperature_code", nullable = false)
+    private TemperatureCode temperatureCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "weather_code", nullable = false)
+    private RainCode rainCode;
+
+    public enum MonthPhase {
+        EARLY,
+        MID,
+        LATE
+    }
+}
