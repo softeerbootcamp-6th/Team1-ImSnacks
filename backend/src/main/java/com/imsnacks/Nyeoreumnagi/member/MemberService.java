e5bdb2aabfb3ab70f1683b026dd3d06f5ef9cf9e
imscow11253 <seaweed8759@naver.com>
2025-08-14 16:47:29 +0900

    feat: 유저 지번 조회 api Service 클래스 생성

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/MemberService.java
@@ -0,0 +1,14 @@
+package com.imsnacks.Nyeoreumnagi.member;
+
+import com.imsnacks.Nyeoreumnagi.member.dto.response.GetMemberAddressResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    public GetMemberAddressResponse getMemberAddress(Long memberId){
+        return null;
+    }
+}
