88dc4f93dacba46c1694c7e5960ba2b1a149d596
eunjin11 <whdmswls9207@gmail.com>
2025-08-14 18:06:45 +0900

    feat: 중첩 시 layer 이동 로직

--- frontend/src/constants/workTimeCoordinate.ts
+++ frontend/src/constants/workTimeCoordinate.ts
@@ -1,7 +1,11 @@
 const WORK_TIME_Y_COORDINATE = {
   1: 35,
   2: 90,
-  3: 155,
+  3: 145,
 };
+
+// y좌표 가져오기
+export const getYCoordinate = (layer: number) =>
+  WORK_TIME_Y_COORDINATE[layer as 1 | 2 | 3] ?? WORK_TIME_Y_COORDINATE[1];
 
 export { WORK_TIME_Y_COORDINATE };
