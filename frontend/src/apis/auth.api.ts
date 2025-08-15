0e0af25a9eb41ce2c959f8e46142a8d79194e446
eunjin11 <whdmswls9207@gmail.com>
2025-08-15 16:47:11 +0900

    feat: login api 연동

--- frontend/src/apis/auth.api.ts
+++ frontend/src/apis/auth.api.ts
@@ -1,5 +1,5 @@
 import HTTP from './http';
 import { LoginRequest, AuthTokens } from '@/types/openapiGenerator';
 
-export const login = (body: LoginRequest) =>
+export const postLogin = (body: LoginRequest) =>
   HTTP.post<LoginRequest, AuthTokens>('/auth/login', body);
