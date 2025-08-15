b708d50cbcf423c63c8fb8dae4d479f5d6d03225
eunjin11 <whdmswls9207@gmail.com>
2025-08-15 21:24:47 +0900

    hotfix: 시간 변경 로직 수정

--- frontend/src/pages/homePage/utils/calculateTimeToPosition.ts
+++ frontend/src/pages/homePage/utils/calculateTimeToPosition.ts
@@ -2,13 +2,13 @@ import dayjs from 'dayjs';
 
 // 시간을 픽셀 위치로 변환하는 함수
 const calculateTimeToPosition = (startTime: string, endTime: string) => {
-  const startDateTime = dayjs(startTime);
-  const endDateTime = dayjs(endTime);
-
   const baseDateTime = dayjs()
     .set('minute', 0)
     .set('second', 0)
     .set('millisecond', 0);
+
+  const startDateTime = dayjs(startTime);
+  const endDateTime = dayjs(endTime);
 
   const durationMinutes = endDateTime.diff(startDateTime, 'minute');
   const relativeMinutes = startDateTime.diff(baseDateTime, 'minute');
