4c0a89e463931ba598d2e0af69726d3f0c5cc174
imscow11253 <seaweed8759@naver.com>
2025-08-15 14:55:09 +0900

    refactor: 유저 못찾았을 때 error status 수정

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/MemberService.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/MemberService.java
@@ -2,15 +2,12 @@ package com.imsnacks.Nyeoreumnagi.member;
 
 import com.imsnacks.Nyeoreumnagi.member.dto.response.GetMemberAddressResponse;
 import com.imsnacks.Nyeoreumnagi.member.entity.Farm;
-import com.imsnacks.Nyeoreumnagi.member.entity.Member;
 import com.imsnacks.Nyeoreumnagi.member.exception.MemberException;
 import com.imsnacks.Nyeoreumnagi.member.repository.FarmRepository;
-import com.imsnacks.Nyeoreumnagi.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus.INVALID_MEMBER_ID;
-import static com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus.NO_FARM_INFO;
+import static com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus.MEMBER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +16,7 @@ public class MemberService {
     private final FarmRepository farmRepository;
 
     public GetMemberAddressResponse getMemberAddress(Long memberId){
-        Farm farm = farmRepository.findByMember_Id(memberId).orElseThrow(() -> new MemberException(INVALID_MEMBER_ID));
+        Farm farm = farmRepository.findByMember_Id(memberId).orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
         return new GetMemberAddressResponse(farm.getState(), farm.getCity(), farm.getTown(), farm.getAddress());
     }
 }
