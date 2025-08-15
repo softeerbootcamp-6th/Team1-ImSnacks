3526956186bb483986fd95c98f33eed7b1e0b07d
adorableco <adorableco.sy@gmail.com>
2025-08-15 15:48:23 +0900

    feat: WeatherJobConfig에 7일 예보 기온 & 날씨상태 Job 세팅

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/WeatherJobConfig.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/WeatherJobConfig.java
@@ -5,7 +5,15 @@ import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.WeatherRea
 import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader.dto.VilageFcstResponseDto;
 import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.writer.WeatherWriter;
 import com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.writer.dto.ShortTermWeatherDto;
+import com.ImSnacks.NyeoreumnagiBatch.seven_days_weather_forecast.*;
+import com.ImSnacks.NyeoreumnagiBatch.processor.WeatherProcessor;
+import com.ImSnacks.NyeoreumnagiBatch.reader.WeatherReader;
+import com.ImSnacks.NyeoreumnagiBatch.seven_days_weather_forecast.dto.*;
+import com.ImSnacks.NyeoreumnagiBatch.reader.dto.VilageFcstResponseDto;
+import com.ImSnacks.NyeoreumnagiBatch.writer.WeatherWriter;
+import com.ImSnacks.NyeoreumnagiBatch.writer.dto.ShortTermWeatherDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -15,13 +23,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class WeatherJobConfig {
     private final WeatherReader weatherReader;
     private final WeatherWriter weatherWriter;
     private final WeatherProcessor weatherProcessor;
-
+    private final SevenDayWeatherConditionReader sevenDayWeatherConditionReader;
+    private final SevenDayWeatherConditionWriter sevenDayWeatherConditionWriter;
+    private final SevenDayWeatherConditionProcessor sevenDayWeatherConditionProcessor;
+    private final SevenDayTemperatureReader sevenDayTemperatureReader;
+    private final SevenDayTemperatureProcessor sevenDayTemperatureProcessor;
+    private final SevenDayTemperatureWriter sevenDayTemperatureWriter;
 
     @Bean
     public Job weatherJob(JobRepository jobRepository, Step weatherStep) {
@@ -31,12 +45,50 @@ public class WeatherJobConfig {
     }
 
     @Bean
+    public Job sevenDayWeatherConditionJob(JobRepository jobRepository, Step sevenDayWeatherConditionStep) {
+        log.info("sevenDayWeatherConditionStep Start");
+        return new JobBuilder("sevenDayWeatherConditionJob", jobRepository)
+                .start(sevenDayWeatherConditionStep)
+                .build();
+    }
+
+    @Bean
+    public Job sevenDayTemperatureJob(JobRepository jobRepository, Step sevenDayTemperatureStep) {
+        log.info("sevenDayWeatherConditionStep Start");
+        return new JobBuilder("sevenDayTemperatureJob", jobRepository)
+                .start(sevenDayTemperatureStep)
+                .build();
+    }
+
+
+
+    @Bean
     public Step weatherStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("weatherStep", jobRepository)
                 .<VilageFcstResponseDto, ShortTermWeatherDto>chunk(100, transactionManager)
                 .reader(weatherReader)
                 .processor(weatherProcessor)
                 .writer(weatherWriter)
+                .build();
+    }
+
+    @Bean
+    public Step sevenDayWeatherConditionStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("sevenDayWeatherConditionStep", jobRepository)
+                .<SevenDayWeatherForecastResponseDto, SevenDayWeatherConditionDto>chunk(10, transactionManager)
+                .reader(sevenDayWeatherConditionReader)
+                .processor(sevenDayWeatherConditionProcessor)
+                .writer(sevenDayWeatherConditionWriter)
+                .build();
+    }
+
+    @Bean
+    public Step sevenDayTemperatureStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("sevenDayTemperatureStep", jobRepository)
+                .<SevenDayTemperatureForecastResponseDto, SevenDayTemperatureForecastDto>chunk(10, transactionManager)
+                .reader(sevenDayTemperatureReader)
+                .processor(sevenDayTemperatureProcessor)
+                .writer(sevenDayTemperatureWriter)
                 .build();
     }
 }
