505d92414a4f1374999d1eba0e518690a7f92739
imscow11253 <seaweed8759@naver.com>
2025-08-15 15:31:42 +0900

    feat: 일일 최대값 batch processor에서 던질 exception 클래스 정의

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/daily_high/exception/DailyHighProcessorException.java
@@ -0,0 +1,7 @@
+package com.ImSnacks.NyeoreumnagiBatch.daily_high.exception;
+
+public class DailyHighProcessorException extends RuntimeException{
+    public DailyHighProcessorException(String message) {
+        super(message);
+    }
+}
