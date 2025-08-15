db726d97f269944d7dc5b6df7d9e415cc6770a63
adorableco <adorableco.sy@gmail.com>
2025-08-15 15:46:37 +0900

    feat: 7일 예보 (날씨상태 용) Writer 구축

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/seven_days_weather_forecast/SevenDayWeatherConditionWriter.java
@@ -0,0 +1,40 @@
+package com.ImSnacks.NyeoreumnagiBatch.seven_days_weather_forecast;
+
+import com.ImSnacks.NyeoreumnagiBatch.seven_days_weather_forecast.dto.SevenDayWeatherConditionDto;
+import com.ImSnacks.NyeoreumnagiBatch.seven_days_weather_forecast.entity.MidTempRegionCode;
+import com.ImSnacks.NyeoreumnagiBatch.seven_days_weather_forecast.repository.MidTempRegionCodeRepository;
+import com.ImSnacks.NyeoreumnagiBatch.writer.repository.SevenDayWeatherForecastRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class SevenDayWeatherConditionWriter implements ItemWriter<SevenDayWeatherConditionDto> {
+    private final SevenDayWeatherForecastRepository sevenDayWeatherForecastRepository;
+    private final MidTempRegionCodeRepository midTempRegionCodeRepository;
+
+    public SevenDayWeatherConditionWriter(SevenDayWeatherForecastRepository sevenDayWeatherForecastRepository, MidTempRegionCodeRepository midTempRegionCodeRepository) {
+        this.sevenDayWeatherForecastRepository = sevenDayWeatherForecastRepository;
+        this.midTempRegionCodeRepository = midTempRegionCodeRepository;
+    }
+
+    @Override
+    @Transactional
+    public void write(Chunk<? extends SevenDayWeatherConditionDto> chunk) {
+        log.info("Write Chunk size: {}", chunk.size());
+
+
+        chunk.getItems().forEach(item -> item.byDayDto().forEach(byDayDto ->
+        {
+            List<MidTempRegionCode> midTempRegionCodes = midTempRegionCodeRepository.findByLandRegionCode_RegId(byDayDto.regionCode());
+            for(MidTempRegionCode code: midTempRegionCodes){
+                sevenDayWeatherForecastRepository.updateWeatherByRegionAndDateTime(code.getRegionCode(), byDayDto.date(), byDayDto.weatherCondition());
+            }
+        }));
+    }
+}
