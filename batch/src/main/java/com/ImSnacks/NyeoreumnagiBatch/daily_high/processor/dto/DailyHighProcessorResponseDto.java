42780f31b306126202e9ea3422e14ce30c9fde06
imscow11253 <seaweed8759@naver.com>
2025-08-15 13:30:42 +0900

    refactor: 일일 최고치 batch job processor에서 반환하는 dto에 nx, ny 값 추가

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/daily_high/processor/dto/DailyHighProcessorResponseDto.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/daily_high/processor/dto/DailyHighProcessorResponseDto.java
@@ -1,10 +1,12 @@
 package com.ImSnacks.NyeoreumnagiBatch.daily_high.processor.dto;
 
 public record DailyHighProcessorResponseDto(
+        int nx,
+        int ny,
         double maxPrecipitation,
         double maxHumidity,
         double maxWindSpeed,
-        double windDirection,
+        int windDirection,
         int maxTemperature
 ) {
 }
