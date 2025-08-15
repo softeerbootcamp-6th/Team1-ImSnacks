6d8132340be75b3c57e09eb7931b4e2297d9c382
adorableco <adorableco.sy@gmail.com>
2025-08-15 15:21:10 +0900

    feat: 7일 예보용 API request values 세팅

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/shortTermWeatherForecast/reader/ApiRequestValues.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/shortTermWeatherForecast/reader/ApiRequestValues.java
@@ -3,13 +3,17 @@ package com.ImSnacks.NyeoreumnagiBatch.shortTermWeatherForecast.reader;
 public enum ApiRequestValues {
     URI("https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstInfoService_2.0/getVilageFcst"),
     AUTH_KEY("authKey"),
+    SERVICE_KEY("serviceKey"),
     NUM_OF_ROWS("numOfRows"),
     PAGE_NO("pageNo"),
     DATA_TYPE("dataType"),
     BASE_DATE("base_date"),
     BASE_TIME("base_time"),
     NX("nx"),
-    NY("ny");
+    NY("ny"),
+
+    MID_RANGE_WEATHER_URI("https://apis.data.go.kr/1360000/MidFcstInfoService")
+    ;
 
     private final String value;
 
