d6a867fb96086fa30849eaa589282cbe2102d082
imscow11253 <seaweed8759@naver.com>
2025-08-15 16:26:37 +0900

    feat: 일일 최대치 batch job 스케쥴링 메소드 구현

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/JobScheduler.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/JobScheduler.java
@@ -31,6 +31,9 @@ public class JobScheduler {
     @Qualifier("sevenDayWeatherConditionJob")
     private final Job sevenDayWeatherConditionJob;
 
+    @Qualifier("dailyHighJob")
+    private final Job dailyHighJob;
+
     //2시부터 3시간 간격으로 15분마다 매일
     @Scheduled(cron = "0 15 2,5,8,11,14,17,20,23 * * *", zone = "Asia/Seoul")
     public void runWeatherJob() throws Exception {
@@ -50,6 +53,13 @@ public class JobScheduler {
     public void runSunRiseSetJob() throws Exception {
         JobParameters params = JobParams.getSunRiseSetJobParam();
         jobLauncher.run(sunRiseSetJob, params);
+    }
+
+    //매일 새벽 0시 1분에 실행
+    @Scheduled(cron = "0 1 0 * * *", zone = "Asia/Seoul")
+    public void runDailyHighJob() throws Exception {
+        JobParameters params = JobParams.getDailyHighJobParam();
+        jobLauncher.run(dailyHighJob, params);
     }
 
     // "매일 오전 6시 30분에 실행"
