203adcf83c8aaf710ab0af8da616a43a42228828
adorableco <adorableco.sy@gmail.com>
2025-08-15 19:47:56 +0900

    feat: 농작업 관련 dto validate() 구현

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/work/controller/MyWorkController.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/work/controller/MyWorkController.java
@@ -20,7 +20,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -38,7 +37,8 @@ public class MyWorkController {
     @ApiResponse(responseCode = "200", description = "농작업 등록 성공")
     @ApiResponse(responseCode = "400", description = "농작업 등록 실패")
     @PostMapping("")
-    public ResponseEntity<CustomResponseBody<RegisterMyWorkResponse>> registerMyWork(@Validated @RequestBody RegisterMyWorkRequest request, @PreAuthorize Long memberId) {
+    public ResponseEntity<CustomResponseBody<RegisterMyWorkResponse>> registerMyWork(@RequestBody RegisterMyWorkRequest request, @PreAuthorize Long memberId) {
+        request.validate();
         RegisterMyWorkResponse dto = myWorkService.registerMyWork(request, memberId);
         return ResponseUtil.success(dto);
     }
@@ -48,7 +48,8 @@ public class MyWorkController {
     @ApiResponse(responseCode = "200", description = "농작업 삭제 성공")
     @ApiResponse(responseCode = "400", description = "농작업 삭제 실패")
     @DeleteMapping("")
-    public ResponseEntity<CustomResponseBody<Void>> deleteMyWork(@Validated @RequestBody DeleteMyWorkRequest request, @PreAuthorize Long memberId) {
+    public ResponseEntity<CustomResponseBody<Void>> deleteMyWork(@RequestBody DeleteMyWorkRequest request, @PreAuthorize Long memberId) {
+        request.validate();
         myWorkService.deleteMyWork(request, memberId);
         return ResponseUtil.success();
     }
@@ -58,7 +59,8 @@ public class MyWorkController {
     @ApiResponse(responseCode = "200", description = "농작업 수정 성공")
     @ApiResponse(responseCode = "400", description = "농작업 수정 실패")
     @PatchMapping("")
-    public ResponseEntity<CustomResponseBody<ModifyMyWorkResponse>> modifyMyWork(@Validated @RequestBody ModifyMyWorkRequest request, @PreAuthorize Long memberId) {
+    public ResponseEntity<CustomResponseBody<ModifyMyWorkResponse>> modifyMyWork(@RequestBody ModifyMyWorkRequest request, @PreAuthorize Long memberId) {
+        request.validate();
         ModifyMyWorkResponse modifyMyWorkResponse = myWorkService.modifyMyWork(request, memberId);
         return ResponseUtil.success(modifyMyWorkResponse);
     }
--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/work/exception/WorkResponseStatus.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/work/exception/WorkResponseStatus.java
@@ -7,7 +7,12 @@ public enum WorkResponseStatus {
     MY_WORK_NOT_FOUND(7004, "해당 작업이 존재하지 않습니다."),
     MY_WORK_NOT_COMPLETED(7005, "해당 작업이 완료되지 않았습니다."),
     LIFE_CYCLE_NOT_FOUND(7006, "일치하는 생육 단계가 존재하지 않습니다."),
-    START_TIME_IS_FUTURE(7011, "요청 시작 시간이 미래입니다.")
+    NULL_MY_CROP_ID(7007, "myCropId가 null입니다."),
+    NULL_START_TIME(7008, "startTime이 null입니다."),
+    NULL_END_TIME(7009, "end이 null입니다."),
+    NULL_MY_WORK_ID(7010, "myWorkId가 null입니다."),
+    START_TIME_IS_FUTURE(7011, "요청 시작 시간이 미래입니다."),
+    NULL_RECOMMENDED_WORK_ID(7012, "recommendedWorkId가 null입니다."),
     ;
 
     private int code;
