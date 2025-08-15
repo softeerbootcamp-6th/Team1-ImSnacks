b602b85e73d03b376bfe44508274186c85fa99b6
adorableco <adorableco.sy@gmail.com>
2025-08-15 15:43:23 +0900

    feat: SevenDayWeatherForecast(엔티티) & SevenDayWeatherForecastId(복합키 클래스)

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/entity/SevenDayWeatherForecast.java
@@ -0,0 +1,23 @@
+package com.ImSnacks.NyeoreumnagiBatch.writer.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(SevenDayWeatherForecastId.class)
+public class SevenDayWeatherForecast {
+    @Id
+    private String regionCode;
+    @Id
+    private LocalDate date;
+    private int maxTemperature;
+    private int minTemperature;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true)
+    private WeatherCondition weatherCondition;
+}
