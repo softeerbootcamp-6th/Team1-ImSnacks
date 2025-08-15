f5e7e730a585dda03638fd799f28566babceb442
lim <gyuhyun.lim@outlook.com>
2025-08-16 02:58:37 +0900

    test: PestRisk 생성 테스트

--- backend/src/test/java/com/imsnacks/Nyeoreumnagi/pest/entity/EntityTest.java
+++ backend/src/test/java/com/imsnacks/Nyeoreumnagi/pest/entity/EntityTest.java
@@ -1,5 +1,7 @@
 package com.imsnacks.Nyeoreumnagi.pest.entity;
 
+import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode;
+import com.imsnacks.Nyeoreumnagi.work.entity.Crop;
 import org.junit.jupiter.api.Test;
 
 import java.time.Month;
@@ -15,10 +17,26 @@ public class EntityTest {
                 .startMonthPhase(PestCondition.MonthPhase.EARLY)
                 .endMonth(Month.AUGUST)
                 .endMonthPhase(PestCondition.MonthPhase.LATE)
+                .humidityCode(WeatherConditionCode.HumidityCode.LOW)
+                .temperatureCode(WeatherConditionCode.TemperatureCode.LOW)
+                .rainCode(WeatherConditionCode.RainCode.NONE)
                 .build();
         assertThat(cond.getStartMonth()).isEqualTo(Month.APRIL);
         assertThat(cond.getStartMonthPhase()).isEqualTo(PestCondition.MonthPhase.EARLY);
         assertThat(cond.getEndMonth()).isEqualTo(Month.AUGUST);
         assertThat(cond.getEndMonthPhase()).isEqualTo(PestCondition.MonthPhase.LATE);
+        assertThat(cond.getHumidityCode()).isEqualTo(WeatherConditionCode.HumidityCode.LOW);
+        assertThat(cond.getTemperatureCode()).isEqualTo(WeatherConditionCode.TemperatureCode.LOW);
+        assertThat(cond.getRainCode()).isEqualTo(WeatherConditionCode.RainCode.NONE);
+    }
+
+    @Test
+    void PestRisk_생성_성공() {
+        PestRisk risk = PestRisk.builder()
+                .name("응애")
+                .build();
+        risk.addCondition(new PestCondition());
+
+        assertThat(risk.getName()).isEqualTo("응애");
     }
 }
