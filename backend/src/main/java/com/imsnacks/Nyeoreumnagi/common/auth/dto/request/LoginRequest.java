9d8396aae4f14b3863fe4642f23e78fe2ce2d423
adorableco <adorableco.sy@gmail.com>
2025-08-15 19:47:48 +0900

    feat: 로그인 관련 dto validate() 구현

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/auth/dto/request/LoginRequest.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/auth/dto/request/LoginRequest.java
@@ -1,11 +1,21 @@
 package com.imsnacks.Nyeoreumnagi.common.auth.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+
+import com.imsnacks.Nyeoreumnagi.member.exception.MemberException;
+
+import static com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus.BLANK_IDENTIFIER;
+import static com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus.BLANK_PASSWORD;
 
 public record LoginRequest(
-        @NotBlank
         String identifier,
-        @NotBlank
         String password
 ) {
+    public void validate(){
+        if(identifier.isBlank()){
+            throw new MemberException(BLANK_IDENTIFIER);
+        }
+        if(password.isBlank()){
+            throw new MemberException(BLANK_PASSWORD);
+        }
+    }
 }
