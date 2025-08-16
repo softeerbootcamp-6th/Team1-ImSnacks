222645d5e20c33805e454f2ab3e7f57e760dbf31
imscow11253 <seaweed8759@naver.com>
2025-08-16 22:05:47 +0900

    update: DashboardWeatherForecastRepository 빈으로 등록하기 위한 어노테이션 추가

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/repository/DashboardWeatherForecastRepository.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/repository/DashboardWeatherForecastRepository.java
@@ -3,6 +3,8 @@ package com.ImSnacks.NyeoreumnagiBatch.common.repository;
 import com.ImSnacks.NyeoreumnagiBatch.common.entity.DashboardWeatherForecast;
 import com.ImSnacks.NyeoreumnagiBatch.common.entity.DashboardWeatherForecastId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DashboardWeatherForecastRepository extends JpaRepository<DashboardWeatherForecast, DashboardWeatherForecastId> {
 }
