b73013710f0a607877b01dcd2e53188f942b32a7
imscow11253 <seaweed8759@naver.com>
2025-08-16 22:04:42 +0900

    feat: DashboardWeatherForecast 엔티티 클래스를 위한 repository interface 생ㅅ어

---
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/repository/DashboardWeatherForecastRepository.java
@@ -0,0 +1,8 @@
+package com.ImSnacks.NyeoreumnagiBatch.common.repository;
+
+import com.ImSnacks.NyeoreumnagiBatch.common.entity.DashboardWeatherForecast;
+import com.ImSnacks.NyeoreumnagiBatch.common.entity.DashboardWeatherForecastId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DashboardWeatherForecastRepository extends JpaRepository<DashboardWeatherForecast, DashboardWeatherForecastId> {
+}
