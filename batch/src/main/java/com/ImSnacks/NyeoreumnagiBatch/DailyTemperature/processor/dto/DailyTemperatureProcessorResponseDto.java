428b878a7077227fbed50825ffac8379b1548d17
imscow11253 <seaweed8759@naver.com>
2025-08-16 20:30:11 +0900

    feat: 시간별 기온 batch processor에서 writer로 전달하는 dto 정의

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/DailyTemperature/processor/dto/DailyTemperatureProcessorResponseDto.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/DailyTemperature/processor/dto/DailyTemperatureProcessorResponseDto.java
@@ -2,11 +2,16 @@ package com.ImSnacks.NyeoreumnagiBatch.DailyTemperature.processor.dto;
 
 import com.ImSnacks.NyeoreumnagiBatch.common.enums.WeatherCondition;
 
+import java.util.List;
+
 public record DailyTemperatureProcessorResponseDto(
         int nx,
         int ny,
-        int fcstTime,
-        int temperature,
-        WeatherCondition weatherCondition
+        List<TemperaturePerTime> temperaturePerTime
 ) {
+    public record TemperaturePerTime(
+            int fcstTime,
+            int temperature,
+            WeatherCondition weatherCondition
+    ){}
 }
