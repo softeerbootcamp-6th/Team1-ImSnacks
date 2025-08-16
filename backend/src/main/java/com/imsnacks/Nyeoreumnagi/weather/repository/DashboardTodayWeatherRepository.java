899905cc8108946e344e5106ff45306414d1c5d8
imscow11253 <seaweed8759@naver.com>
2025-08-16 17:38:25 +0900

    feat: 강수량 projection을 위한 repository 메소드 생성

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/DashboardTodayWeatherRepository.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/repository/DashboardTodayWeatherRepository.java
@@ -2,10 +2,7 @@ package com.imsnacks.Nyeoreumnagi.weather.repository;
 
 import com.imsnacks.Nyeoreumnagi.weather.entity.DashboardTodayWeather;
 import com.imsnacks.Nyeoreumnagi.weather.entity.DashboardTodayWeatherId;
-import com.imsnacks.Nyeoreumnagi.weather.service.projection_entity.HumidityInfo;
-import com.imsnacks.Nyeoreumnagi.weather.service.projection_entity.SunriseSunSetTime;
-import com.imsnacks.Nyeoreumnagi.weather.service.projection_entity.UVInfo;
-import com.imsnacks.Nyeoreumnagi.weather.service.projection_entity.WindInfo;
+import com.imsnacks.Nyeoreumnagi.weather.service.projection_entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -15,4 +12,5 @@ public interface DashboardTodayWeatherRepository extends JpaRepository<Dashboard
     Optional<UVInfo> findUVByNxAndNy(int nx, int ny);
     Optional<WindInfo> findWindByNxAndNy(int nx, int ny);
     Optional<HumidityInfo> findHumidityByNxAndNy(int nx, int ny);
+    Optional<PrecipitationInfo> findPrecipitationByNxAndNy(int nx, int ny);
 }
