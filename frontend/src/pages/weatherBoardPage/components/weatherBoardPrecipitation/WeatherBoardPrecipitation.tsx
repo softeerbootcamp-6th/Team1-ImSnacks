7600b1440537dfc4be5f088c06fb57e4e085d204
bamb14 <118671229+bamb14@users.noreply.github.com>
2025-08-17 15:09:33 +0900

    feat: 최고 강수량 조회 api 연동 및 데이터 fetch

--- frontend/src/pages/weatherBoardPage/components/weatherBoardPrecipitation/WeatherBoardPrecipitation.tsx
+++ frontend/src/pages/weatherBoardPage/components/weatherBoardPrecipitation/WeatherBoardPrecipitation.tsx
@@ -1,15 +1,39 @@
+import type { GetDailyMaxPrecipitationResponse } from '@/types/openapiGenerator';
 import { usePrecipitationSvg } from '../../hooks/usePrecipitationSvg';
 import S from './WeatherBoardPrecipitation.style';
+import { useEffect, useState } from 'react';
+import { getWeatherPrecipitationInfo } from '@/apis/weather.api';
 
-const WeatherBoardPrecipitation = ({ value }: { value: number }) => {
-  const { svgElement } = usePrecipitationSvg({ value });
+const WeatherBoardPrecipitation = () => {
+  const [maxPrecipitation, setMaxPrecipitation] =
+    useState<GetDailyMaxPrecipitationResponse>();
+
+  const fetchMaxPrecipitation = async () => {
+    try {
+      const res = await getWeatherPrecipitationInfo();
+      if (res.data) {
+        setMaxPrecipitation(res.data);
+      }
+    } catch (error) {
+      console.error('Error fetching max precipitation:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMaxPrecipitation();
+  }, []);
+
+  const { svgElement } = usePrecipitationSvg({
+    value: maxPrecipitation?.value ?? 0,
+  });
 
   return (
     <div css={S.WeatherBoardPrecipitation}>
       <div css={S.WeatherBoardPrecipitationTitle}>
         <h3>최고 강수량</h3>
         <p>
-          {value}mm {value >= 30 && '이상'}
+          {maxPrecipitation?.value}mm{' '}
+          {(maxPrecipitation?.value ?? 0) >= 30 && '이상'}
         </p>
       </div>
 
