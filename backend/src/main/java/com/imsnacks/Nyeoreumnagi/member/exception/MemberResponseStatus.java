6b1e7ecc91e081872e443609a265c71e1543723a
adorableco <adorableco.sy@gmail.com>
2025-08-17 17:00:40 +0900

    feat: MemberResponseStatus 에러 코드 추가 (2006~2011)

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/exception/MemberResponseStatus.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/exception/MemberResponseStatus.java
@@ -6,7 +6,13 @@ public enum MemberResponseStatus {
     NO_FARM_INFO(2003, "해당 멤버는 농장 정보가 없습니다."),
     INCORRECT_PASSWORD(2004, "비밀번호가 옳지 않습니다."),
     BLANK_IDENTIFIER(2005, "identifer가 비어있습니다."),
-    BLANK_PASSWORD(2005, "password가 비어있습니다.")
+    BLANK_PASSWORD(2005, "password가 비어있습니다."),
+    BLANK_NICKNAME(2006, "nickname이 비어있습니다."),
+    INVALID_PHONE_NUMBER_FORMAT(2007, "전화번호 형식은 '010-1234-5678'입니다."),
+    BLANK_FARM_STATE(2008, "농장 광역시도 정보가 비어있습니다."),
+    BLANK_FARM_CITY(2009, "농장 시군구 정보가 비어있습니다."),
+    BLANK_FARM_TOWN(2010, "농장 읍면동 정보가 비어있습니다."),
+    BLANK_FARM_ADDRESS(2011, "농장 번지 번호가 비어있습니다."),
     ;
 
     private int code;
