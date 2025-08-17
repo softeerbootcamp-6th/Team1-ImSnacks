7952c0fd6e89c2b76ba90b54a67e33e87b2bd555
imscow11253 <seaweed8759@naver.com>
2025-08-17 16:20:15 +0900

    feat: 기상 그래프 조회 API response dto 필드값 추가

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/dto/response/GetWeatherGraphResponse.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/dto/response/GetWeatherGraphResponse.java
@@ -2,12 +2,15 @@ package com.imsnacks.Nyeoreumnagi.weather.dto.response;
 
 import com.imsnacks.Nyeoreumnagi.common.enums.WeatherMetric;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record GetWeatherGraphResponse(
         int max,
         int min,
         WeatherMetric weatherMetric,
+        boolean isUpdated,
+        LocalDateTime lastUpdateTime,
         List<ValuePerTime> valuePerTime
 ){
     public record ValuePerTime(
