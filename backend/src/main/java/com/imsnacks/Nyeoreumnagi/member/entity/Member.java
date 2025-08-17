549bb64c3ea6492db80f9b08db1f165a68ee65a4
adorableco <adorableco.sy@gmail.com>
2025-08-17 21:12:33 +0900

    fix: 농작업 업데이트 시 불필요한 RecommendedWork, Farm 조회를 피하기 위해 FetchType.LAZY 설정

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/entity/Member.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/member/entity/Member.java
@@ -2,7 +2,10 @@ package com.imsnacks.Nyeoreumnagi.member.entity;
 
 import com.imsnacks.Nyeoreumnagi.farm.entity.Farm;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
