1b1bdd6fffeb9fcd86e5cbf12da87a41dac72fd9
lim <gyuhyun.lim@outlook.com>
2025-08-17 20:43:38 +0900

    chore: OrderByIdAsc -> OrderById

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/pest/service/PestService.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/pest/service/PestService.java
@@ -5,11 +5,10 @@ import com.imsnacks.Nyeoreumnagi.member.exception.MemberException;
 import com.imsnacks.Nyeoreumnagi.member.exception.MemberResponseStatus;
 import com.imsnacks.Nyeoreumnagi.member.repository.FarmRepository;
 import com.imsnacks.Nyeoreumnagi.pest.dto.response.GetPestCardListResponse;
+import com.imsnacks.Nyeoreumnagi.pest.dto.response.GetPestCardListResponse.MyCropCard;
+import com.imsnacks.Nyeoreumnagi.pest.dto.response.GetPestCardListResponse.PestCard;
 import com.imsnacks.Nyeoreumnagi.pest.entity.PestCondition;
 import com.imsnacks.Nyeoreumnagi.pest.entity.PestRisk;
-import com.imsnacks.Nyeoreumnagi.pest.dto.response.GetPestCardListResponse.PestCard;
-import com.imsnacks.Nyeoreumnagi.pest.dto.response.GetPestCardListResponse.MyCropCard;
-
 import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode.HumidityLevel;
 import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode.RainLevel;
 import com.imsnacks.Nyeoreumnagi.pest.service.WeatherConditionCode.TemperatureLevel;
@@ -28,8 +27,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Service
@@ -42,24 +41,55 @@ public class PestService {
 
     public GetPestCardListResponse getPestCardList(final Long memberId, @Nullable final Long myCropId) {
         final Farm farm = farmRepo.findByMember_Id(memberId).orElseThrow(() -> new MemberException(MemberResponseStatus.NO_FARM_INFO));
-        final var forecastList = fcstRepo.findAllByNxAndNy(farm.getNx(), farm.getNy());
-        if (forecastList.isEmpty()) {
-            throw new WeatherException(WeatherResponseStatus.NO_WEATHER_LOCATION);
-        }
-        final WeatherConditionCode currentWeather = WeatherConditionCode.of(forecastList);
-        final LocalDateTime now = LocalDateTime.now();
+        final Crop target = (myCropId == null) ? findDefaultCrop(memberId) : findCropBy(myCropId);
+        final List<PestCard> pestCards = getPestCardsFor(target, farm.getNx(), farm.getNy());
+        final List<MyCropCard> myCropCards = (myCropId == null) ? getMyCropCards(memberId) : new ArrayList<>();
 
+        return new GetPestCardListResponse(pestCards, myCropCards);
+    }
+
+    private List<MyCropCard> getMyCropCards(long memberId) {
+        List<MyCrop> myCrops = this.myCropRepo.findAllByMember_IdOrderById(memberId);
+        return myCrops.stream()
+                .map(x -> new MyCropCard(x.getId(), x.getCrop().getName()))
+                .toList();
+    }
+
+    private Crop findDefaultCrop(long memberId) {
+        List<MyCrop> myCrops = myCropRepo.findAllByMember_IdOrderByCrop_Id(memberId);
+        if (myCrops.isEmpty()) {
+            //TODO 빈 리스트를 보내야 할까, 아니면 예외를 던져야 할까?
+            throw new CropException(CropResponseStatus.MY_CROP_NOT_FOUND);
+        }
+        Crop crop = myCrops.get(0).getCrop();
+        if (crop == null) {
+            throw new CropException(CropResponseStatus.MY_CROP_NOT_FOUND);
+        }
+        return crop;
+    }
+
+    private Crop findCropBy(@NotNull long myCropId) {
         final MyCrop myCrop = myCropRepo.findById(myCropId).orElseThrow(() -> new CropException(CropResponseStatus.MY_CROP_NOT_FOUND));
         final Crop crop = myCrop.getCrop();
         if (crop == null) {
             throw new CropException(CropResponseStatus.MY_CROP_NOT_FOUND);
         }
-        final List<PestRisk> relatedRisks = crop.getPestRisks();
-        final Stream<PestRisk> filtered = relatedRisks.stream()
-                .filter(x -> meetsCondition(x.getConditions(), currentWeather, now));
-        final List<PestCard> pestCards = filtered.map(x -> x.toCard()).limit(MAX_PEST_CARD_LIST_SIZE).toList();
+        return crop;
+    }
 
-        return new GetPestCardListResponse(pestCards, null);
+    private List<PestCard> getPestCardsFor(Crop target, int nx, int ny) {
+        List<ShortTermWeatherForecast> forecastList = fcstRepo.findAllByNxAndNy(nx, ny);
+        if (forecastList.isEmpty()) {
+            throw new WeatherException(WeatherResponseStatus.NO_WEATHER_LOCATION);
+        }
+        WeatherConditionCode forecast = WeatherConditionCode.of(forecastList);
+        LocalDateTime now = LocalDateTime.now();
+        List<PestRisk> relatedPests = target.getPestRisks();
+        return relatedPests.stream()
+                .filter(x -> meetsCondition(x.getConditions(), forecast, now))
+                .limit(MAX_PEST_CARD_LIST_SIZE)
+                .map(x -> x.toCard())
+                .toList();
     }
 
     private boolean meetsCondition(final List<PestCondition> pestConditions, final WeatherConditionCode weatherConditionCode, final LocalDateTime now) {
