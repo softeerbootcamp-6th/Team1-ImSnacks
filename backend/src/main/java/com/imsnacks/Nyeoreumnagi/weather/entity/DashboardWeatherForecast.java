5724e36e401e7320c29ff384617c2a9f9104d4e8
imscow11253 <seaweed8759@naver.com>
2025-08-17 14:22:41 +0900

    feat: DashboardWeatherForecast 엔티티 생성

---
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/weather/entity/DashboardWeatherForecast.java
@@ -0,0 +1,27 @@
+package com.imsnacks.Nyeoreumnagi.weather.entity;
+
+import com.imsnacks.Nyeoreumnagi.common.enums.WeatherCondition;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(DashboardWeatherForecastId.class)
+public class DashboardWeatherForecast {
+    @Id
+    private int nx;
+    @Id
+    private int ny;
+    @Id
+    @Column(name = "fcst_time")
+    private int fcstTime;
+
+    @Column(name = "temperature", nullable = false)
+    Integer temperature;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sky_status", nullable = false)
+    WeatherCondition weatherCondition;
+}
