01700a80216a78f0f4b5e6763d1432b9774fb1ce
bamb14 <althcjstk08@gmail.com>
2025-08-18 17:00:17 +0900

    feat: MobileTodo 조회 api 연동 및 데이터 fetch

--- frontend/src/pages/homePage/components/mobileTodo/MobileTodo.tsx
+++ frontend/src/pages/homePage/components/mobileTodo/MobileTodo.tsx
@@ -1,33 +1,41 @@
 import type { WorkChipType } from '@/types/workChip.type';
 import MobileWorkCard from '../mobileWorkCard/MobileWorkCard';
 import S from './MobileTodo.style';
+import { useEffect, useState } from 'react';
+import { getMyWorkOfToday } from '@/apis/myWork.api';
+import type { GetMyWorksOfTodayResponse } from '@/types/openapiGenerator';
 
-interface MobileTodoProps {
-  id: number;
-  cropName: string;
-  workName: string;
-  workTime: string;
-  status: string;
-}
+const MobileTodo = () => {
+  const [todoList, setTodoList] = useState<GetMyWorksOfTodayResponse[]>();
 
-interface MobileTodoComponentProps {
-  todoList: MobileTodoProps[];
-}
+  const fetchTodayWork = async () => {
+    try {
+      const res = await getMyWorkOfToday(true);
+      if (res.data) {
+        setTodoList(res.data);
+        console.log('오늘의 작업:', res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching today's work:", error);
+    }
+  };
+  useEffect(() => {
+    fetchTodayWork();
+  }, []);
 
-const MobileTodo = ({ todoList }: MobileTodoComponentProps) => {
   return (
     <div css={S.MobileTodo}>
       <h2>오늘 예정된 할 일이에요</h2>
       <div css={S.MobileTodoList}>
-        {todoList.map(todo => (
+        {todoList?.map(todo => (
           <MobileWorkCard
-            key={todo.id}
-            workId={todo.id}
-            cropName={todo.cropName}
-            workName={todo.workName}
-            workTime={todo.workTime}
-            status={todo.status as WorkChipType}
-            onClick={() => console.log(`Work ID: ${todo.id}`)}
+            key={todo?.myWorkId}
+            workId={todo?.myWorkId}
+            cropName={todo?.myCropName ?? ''}
+            workName={todo?.myWorkName ?? ''}
+            workTime={todo?.workTime ?? ''}
+            status={todo?.status as WorkChipType}
+            onClick={() => console.log(`Work ID: ${todo?.myWorkId}`)}
           />
         ))}
       </div>
