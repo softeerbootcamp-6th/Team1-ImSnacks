dbb5db6eea923a641c554a6f8e955a7a4e467129
bamb14 <althcjstk08@gmail.com>
2025-08-18 16:33:24 +0900

    feat: 날씨 요약 조회 api 연동 및 데이터 fetch

--- frontend/src/pages/homePage/mobile/MobileHomePage.tsx
+++ frontend/src/pages/homePage/mobile/MobileHomePage.tsx
@@ -1,11 +1,36 @@
-import { WEATHER_CONDITIONS, WEATHER_METRICS } from '@/types/weather.types';
+import {
+  WEATHER_CONDITIONS,
+  WEATHER_METRICS,
+  type WeatherConditionsType,
+} from '@/types/weather.types';
 import MobileHeader from '../components/mobileHeader/MobileHeader';
 import S from './MobileHomePage.style';
 import MobileHeadline from '../components/mobileHeadline/MobileHeadline';
 import MobileCurrentWeather from '../components/mobileCurrentWeather/MobileCurrentWeather';
 import MobileTodo from '../components/mobileTodo/MobileTodo';
+import { useEffect, useState } from 'react';
+import { getWeatherNow } from '@/apis/weather.api';
+import type { GetWeatherConditionResponse } from '@/types/openapiGenerator';
 
 const MobileHomePage = () => {
+  const [weatherSummary, setWeatherSummary] =
+    useState<GetWeatherConditionResponse>();
+
+  const fetchWeather = async () => {
+    try {
+      const res = await getWeatherNow();
+      if (res.data) {
+        setWeatherSummary(res.data);
+      }
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
   const todoList = [
     {
       id: 1,
@@ -32,9 +57,12 @@ const MobileHomePage = () => {
   return (
     <div css={S.MobileHomePage}>
       <MobileHeader
-        weatherCondition={WEATHER_CONDITIONS.CLOUDY}
-        weatherKeyword="구름 많음"
-        temperature={25}
+        weatherCondition={
+          (weatherSummary?.weatherCondition as WeatherConditionsType) ??
+          WEATHER_CONDITIONS.SUNNY
+        }
+        weatherKeyword={weatherSummary?.weatherKeyword ?? '맑음'}
+        temperature={weatherSummary?.temperature ?? 25}
       />
       <div css={S.MobileHomeContentWrapper}>
         <MobileHeadline />
