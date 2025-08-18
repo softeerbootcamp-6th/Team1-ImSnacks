a07eb85dda84f7ff31d434c715ae97d7f0be20cb
bamb14 <althcjstk08@gmail.com>
2025-08-18 17:28:04 +0900

    feat: 내 농작업 상태 변경 API 적용 및 데이터 fetch

--- frontend/src/pages/homePage/components/mobileTodo/MobileTodo.tsx
+++ frontend/src/pages/homePage/components/mobileTodo/MobileTodo.tsx
@@ -1,9 +1,11 @@
-import type { WorkChipType } from '@/types/workChip.type';
 import MobileWorkCard from '../mobileWorkCard/MobileWorkCard';
 import S from './MobileTodo.style';
 import { useEffect, useState } from 'react';
-import { getMyWorkOfToday } from '@/apis/myWork.api';
-import type { GetMyWorksOfTodayResponse } from '@/types/openapiGenerator';
+import { getMyWorkOfToday, patchMyWorkStatus } from '@/apis/myWork.api';
+import {
+  UpdateMyWorkStatusRequestStatusEnum,
+  type GetMyWorksOfTodayResponse,
+} from '@/types/openapiGenerator';
 
 const MobileTodo = () => {
   const [todoList, setTodoList] = useState<GetMyWorksOfTodayResponse[]>();
@@ -13,7 +15,6 @@ const MobileTodo = () => {
       const res = await getMyWorkOfToday(true);
       if (res.data) {
         setTodoList(res.data);
-        console.log('오늘의 작업:', res.data);
       }
     } catch (error) {
       console.error("Error fetching today's work:", error);
@@ -23,19 +24,55 @@ const MobileTodo = () => {
     fetchTodayWork();
   }, []);
 
+  const handleCheckButton = async (
+    myWorkId: number,
+    status: UpdateMyWorkStatusRequestStatusEnum
+  ) => {
+    try {
+      const changedStatus =
+        status === UpdateMyWorkStatusRequestStatusEnum.Incompleted
+          ? UpdateMyWorkStatusRequestStatusEnum.Completed
+          : UpdateMyWorkStatusRequestStatusEnum.Incompleted;
+
+      const res = await patchMyWorkStatus({ myWorkId, status: changedStatus });
+
+      if (res.code === 200) {
+        setTodoList(prevList => {
+          const updatedList = prevList?.map(todo =>
+            todo.myWorkId === myWorkId
+              ? { ...todo, status: changedStatus }
+              : todo
+          );
+          return updatedList;
+        });
+      }
+    } catch (error) {
+      console.error('Error updating work status:', error);
+    }
+  };
+
   return (
     <div css={S.MobileTodo}>
       <h2>오늘 예정된 할 일이에요</h2>
       <div css={S.MobileTodoList}>
-        {todoList?.map(todo => (
+        {todoList?.map((todo: GetMyWorksOfTodayResponse) => (
           <MobileWorkCard
             key={todo?.myWorkId}
             workId={todo?.myWorkId}
             cropName={todo?.myCropName ?? ''}
             workName={todo?.myWorkName ?? ''}
             workTime={todo?.workTime ?? ''}
-            status={todo?.status as WorkChipType}
-            onClick={() => console.log(`Work ID: ${todo?.myWorkId}`)}
+            status={
+              todo?.status ?? UpdateMyWorkStatusRequestStatusEnum.Incompleted
+            }
+            onClick={() => {
+              if (
+                typeof todo?.myWorkId === 'number' &&
+                typeof todo?.status === 'string'
+              ) {
+                handleCheckButton(todo.myWorkId, todo.status);
+              }
+            }}
           />
         ))}
       </div>
