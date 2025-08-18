ce4752fd1ddc9ef456905cffdaae5e4a24fbcbf8
imscow11253 <seaweed8759@naver.com>
2025-08-18 10:32:17 +0900

    refactor: Jpa repository 인터페이스에 Repository 어노테이션 삭제

--- batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/repository/DashboardWeatherForecastRepository.java
+++ batch/src/main/java/com/ImSnacks/NyeoreumnagiBatch/common/repository/DashboardWeatherForecastRepository.java
@@ -5,6 +5,5 @@ import com.ImSnacks.NyeoreumnagiBatch.common.entity.DashboardWeatherForecastId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DashboardWeatherForecastRepository extends JpaRepository<DashboardWeatherForecast, DashboardWeatherForecastId> {
 }
