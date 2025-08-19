9df56dc6196bef3b6b1d31250e49183f97da15a4
adorableco <tpdusdl1001@gmail.com>
2025-08-19 17:00:53 +0900

    feat: 쿠키 세팅이 가능한 ResponseUtil.success() 메서드 구성

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/response/ResponseUtil.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/response/ResponseUtil.java
@@ -1,12 +1,27 @@
 package com.imsnacks.Nyeoreumnagi.common.response;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 public class ResponseUtil {
 
     public static <T> ResponseEntity<CustomResponseBody<T>> success(T data) {
         return ResponseEntity.ok(new CustomResponseBody<>(StatusCode.SUCCESS.getCode(),StatusCode.SUCCESS.getMessage(), data));
+    }
+
+    public static <T> ResponseEntity<CustomResponseBody<T>> success(T data, UUID refreshToken) {
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken.toString())
+                .path("/")
+                .maxAge(86400 * 3)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new CustomResponseBody<>(StatusCode.SUCCESS.getCode(),StatusCode.SUCCESS.getMessage(), data));
     }
 
     public static <T> ResponseEntity<CustomResponseBody<T>> success() {
