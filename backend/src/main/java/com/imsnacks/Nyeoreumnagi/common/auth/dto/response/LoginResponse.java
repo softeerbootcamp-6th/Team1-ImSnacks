43a0e1c8d166e5f505cb4f1f499afdc94e14318c
adorableco <tpdusdl1001@gmail.com>
2025-08-19 17:00:34 +0900

    fix: LoginResponse 구조 수정

--- backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/auth/dto/response/LoginResponse.java
+++ backend/src/main/java/com/imsnacks/Nyeoreumnagi/common/auth/dto/response/LoginResponse.java
@@ -1,10 +1,14 @@
 package com.imsnacks.Nyeoreumnagi.common.auth.dto.response;
 
+
 import java.util.UUID;
 
 public record LoginResponse(
-        String nickname,
-        String accessToken,
-        UUID refreshToken
+        UUID refreshToken,
+        LoginAccessTokenResponse loginAccessTokenResponse
 ) {
+    public record LoginAccessTokenResponse(
+            String nickname,
+            String accessToken
+    ){}
 }
