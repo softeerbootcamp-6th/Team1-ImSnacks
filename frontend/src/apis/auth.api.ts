92dc9f863ce1539249357381dbd134ff9e8bf6ea
bamb14 <althcjstk08@gmail.com>
2025-08-19 20:49:09 +0900

    feat: 단순 fetch AuthRefresh api 정의

--- frontend/src/apis/auth.api.ts
+++ frontend/src/apis/auth.api.ts
@@ -1,5 +1,25 @@
+import { ApiError } from './ApiError';
 import HTTP from './http';
 import { LoginRequest, LoginResponse } from '@/types/openapiGenerator';
 
 export const postLogin = (body: LoginRequest) =>
   HTTP.post<LoginRequest, LoginResponse>('/auth/login', body);
+
+export const getAuthRefresh = async () => {
+  const baseUrl = import.meta.env.VITE_API_URL;
+  const response = await fetch(`${baseUrl}/auth/refresh`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    const errorBody = await response.json();
+    throw new ApiError(
+      response.status,
+      errorBody.code,
+      errorBody.msg,
+      errorBody.data
+    );
+  }
+  return response.json();
+};
