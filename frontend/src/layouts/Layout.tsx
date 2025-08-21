f834bcc01fa44c8617499816bae384608a73f85c
bamb14 <althcjstk08@gmail.com>
2025-08-21 18:34:57 +0900

    chore: Layout.tsx min-width, max-width 수정

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -64,6 +64,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
       <main
         css={css`
           max-width: 1328px;
+          min-width: 1328px;
           position: relative;
           z-index: 1;
           width: 100%;
