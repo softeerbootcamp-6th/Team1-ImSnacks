c640f625ca3bc39a4a500db55539b0f397333936
bamb14 <althcjstk08@gmail.com>
2025-08-22 13:41:08 +0900

    feat: formatRelativeTime 유틸 함수 구현

--- frontend/src/utils/formatTimeUtil.ts
+++ frontend/src/utils/formatTimeUtil.ts
@@ -1,4 +1,4 @@
-import type dayjs from 'dayjs';
+import dayjs from 'dayjs';
 
 export const formatCurrentTime = (currentTime: dayjs.Dayjs) => {
   return {
@@ -6,3 +6,25 @@ export const formatCurrentTime = (currentTime: dayjs.Dayjs) => {
     time: currentTime.format('h:mm A'),
   };
 };
+
+export function formatRelativeTime(
+  targetDate: Date | string,
+  currentTime: dayjs.Dayjs
+): string {
+  const target = dayjs(targetDate);
+  const diffSeconds = currentTime.diff(target, 'second');
+
+  if (diffSeconds < 60) {
+    return `${diffSeconds}초 전`;
+  }
+  const diffMinutes = currentTime.diff(target, 'minute');
+  if (diffMinutes < 60) {
+    return `${diffMinutes}분 전`;
+  }
+  const diffHours = currentTime.diff(target, 'hour');
+  if (diffHours < 24) {
+    return `${diffHours}시간 전`;
+  }
+  const diffDays = currentTime.diff(target, 'day');
+  return `${diffDays}일 전`;
+}
