2d11fe9515add30434c044ee1e58c35a322327a3
eunjin11 <whdmswls9207@gmail.com>
2025-08-23 14:30:05 +0900

    fix: 로그인 페이지 이메일->아이디 수정

--- frontend/src/pages/loginPage/LoginPage.tsx
+++ frontend/src/pages/loginPage/LoginPage.tsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
           <form css={S.LoginForm} onSubmit={handleSubmit}>
             <div css={S.LoginInputGroup}>
               <label css={S.LoginLabel} htmlFor="identifier">
-                이메일
+                아이디
               </label>
               <input
                 id="identifier"
