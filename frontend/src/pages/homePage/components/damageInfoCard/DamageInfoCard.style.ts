42b907db6e4940eb9d88e51641a604116de18ca5
eunjin11 <whdmswls9207@gmail.com>
2025-08-24 16:57:57 +0900

    feat: 스타일 코드 분리

--- frontend/src/pages/homePage/components/damageInfoCard/DamageInfoCard.style.ts
+++ frontend/src/pages/homePage/components/damageInfoCard/DamageInfoCard.style.ts
@@ -2,6 +2,8 @@ import { BorderRadius } from '@/styles/borderRadius';
 import { Opacity } from '@/styles/colors';
 import { customBorderGradientStyles } from '@/styles/customBorderGradientStyles';
 import { Spacing } from '@/styles/spacing';
+import { Typography } from '@/styles/typography';
+import { Assets } from '@/styles/colors';
 import { css } from '@emotion/react';
 
 const DamageInfoCard = css`
@@ -20,6 +22,26 @@ const DamageInfoCard = css`
   background: ${Opacity.White.W500};
 `;
 
+const DamageInfoCardTitle = css`
+  ${Typography.Body_L_700};
+  color: ${Assets.Text.Global.Headline};
+`;
+
+const DamageInfoCardContent = css`
+  ${Typography.Body_M_400};
+  color: ${Assets.Text.Global.Headline};
+`;
+
+const DamagaMyCropsContainer = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  flex-wrap: wrap;
+`;
+
 export default {
   DamageInfoCard,
+  DamageInfoCardTitle,
+  DamageInfoCardContent,
+  DamagaMyCropsContainer,
 };
